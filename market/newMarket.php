a3feead419bb938abe9063864397983a8303c895
zsalwen <zach@mdwestserve.com>
2010-10-21 10:23:55 -0700

    ''

--- market/newMarket.php
+++ market/newMarket.php
@@ -3,7 +3,7 @@
 mysql_connect();
 mysql_select_db('core');
 if ($_POST[submit]){
-	@mysql_query("INSERT INTO market (type, name, contact, phone, address, createDate, createID) VALUES ('$_POST[type]','$_POST[name]','$_POST[contact]','$_POST[phone]', $_POST[address], NOW(), '".$_COOKIE[psdata][user_id]."')") or die (mysql_error());
+	@mysql_query("INSERT INTO market (type, name, contact, phone, address, createDate, createID) VALUES ('$_POST[type]','$_POST[name]','$_POST[contact]','$_POST[phone]', '$_POST[address]', NOW(), '".$_COOKIE[psdata][user_id]."')") or die (mysql_error());
 	echo "<table><tr><td>A new user has been created:<br>ID: ".mysql_insert_id()."<br>Type: $_POST[type]<br>Name: $_POST[name]<br>Contact: $_POST[contact]<br>Phone: $_POST[phone]";
 	error_log("[".date('h:iA n/j/y')."] ".$_COOKIE[psdata][name]." CREATED NEW MARKETING CONTACT FOR ".strtoupper($_POST[type])." ".strtoupper($_POST[name]),3,"/logs/user.log");
 }
