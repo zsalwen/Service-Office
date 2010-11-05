8d7177513bb50a1d496cb617bce561661d014351
zsalwen <zach@mdwestserve.com>
2010-11-05 10:03:53 -0700

    Correcting $h3a & $h3b overwrite (should be $h4a & $h4b)

--- market/index.php
+++ market/index.php
@@ -45,8 +45,8 @@ while ($d4=mysql_fetch_array($r3,MYSQL_ASSOC)){
 	$h4 .= "<li><a href='details.php?id=$d3[marketID]'>$d3[name]</a></li>";
 	$phClass=explode(' ',$d4[phase]);
 	$phClass=$phClass[0];
-	$h3a .= "<li class='$phClass'>$d3[phase]-$d3[callBack]</li>";
-	$h3b .= "<li class='$phClass'>$d3[coldCall]</li>";
+	$h4a .= "<li class='$phClass'>$d3[phase]-$d3[callBack]</li>";
+	$h4b .= "<li class='$phClass'>$d3[coldCall]</li>";
 }
 $today=date('m/d/Y');
 if ($_GET[msg]){
