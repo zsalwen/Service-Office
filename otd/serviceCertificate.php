0b10a14366732c0b4c3d27b5f2422962d3a35110
zsalwen <zach@mdwestserve.com>
2010-11-16 13:10:29 -0800

    Further spacing

--- otd/serviceCertificate.php
+++ otd/serviceCertificate.php
@@ -28,7 +28,7 @@ if ($d[altPlaintiff] != ''){
 }else{
 	$plaintiff=str_replace("-","<br>",$d1[ps_plaintiff]);
 }
-$header="<td colspan='2' align='center' style='font-size:20px; line-height:15px;'>CIRCUIT COURT OF $court, MARYLAND</td></tr>
+$header="<td colspan='2' align='center' style='font-size:20px; line-height:20px;'>CIRCUIT COURT OF $court, MARYLAND</td></tr>
 		<tr></tr>
 		<tr><td class='a' width='550px'><span style='border-bottom:1px solid black;'>$plaintiff</span><small><br /><em>Plaintiff</em></small><br /><br />v.<br /><br />";
 			$header .= strtoupper($d['name1']).'<br>';
@@ -53,7 +53,7 @@ table {page-break-after:always;}
 <center><div style="width: 700px;">
 <table align="center" width="100%" border="0">
 <tr><?=$header?><IMG SRC='http://staff.mdwestserve.com/barcode.php?barcode=<?=$cord?>&width=350&height=40'><center>File Number: <?=$d[client_file]?><br>Set 1</center></td></tr>
-<tr><td colspan='2' class='b' align='center' style='line-height:30px;'><?=$title?></td></tr>
+<tr><td colspan='2' class='b' align='center' style='line-height:40px;'><?=$title?></td></tr>
 <tr><td colspan='2' style='text-indent:20px; line-height:15px; font-size:14px;'><?=$text?></td></tr>
 <tr><td colspan='2' style="font-weight:bold; padding-left:25px;"><?=$mailing?></td></tr>
 <tr><td></td width='50%'><td align='left'><div style='width:250px; height:30px; border-bottom:1px solid black;'>&nbsp;</div>
