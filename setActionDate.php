4bf6d76e8b4a2dcdb473448133f14ef4f9e5be46
zsalwen <zach@mdwestserve.com>
2010-11-17 10:29:13 -0500

    Adding to Repository

---
+++ setActionDate.php
@@ -0,0 +1,175 @@
+<?
+include 'common.php';
+function monthConvert($month){
+	if ($month == '01'){ return 'January'; }
+	if ($month == '02'){ return 'February'; }
+	if ($month == '03'){ return 'March'; }
+	if ($month == '04'){ return 'April'; }
+	if ($month == '05'){ return 'May'; }
+	if ($month == '06'){ return 'June'; }
+	if ($month == '07'){ return 'July'; }
+	if ($month == '08'){ return 'August'; }
+	if ($month == '09'){ return 'September'; }
+	if ($month == '10'){ return 'October'; }
+	if ($month == '11'){ return 'November'; }
+	if ($month == '12'){ return 'December'; }
+}
+function month2num($month){
+	if (strtoupper($month) == 'JANUARY' || $month == 1){
+		return '1';
+	}elseif (strtoupper($month) == 'FEBRUARY' || $month == 2){
+		return '2';
+	}elseif (strtoupper($month) == 'MARCH' || $month == 3){
+		return '3';
+	}elseif (strtoupper($month) == 'APRIL' || $month == 4){
+		return '4';
+	}elseif (strtoupper($month) == 'MAY' || $month == 5){
+		return '5'; 
+	}elseif (strtoupper($month) == 'JUNE' || $month == 6){
+		return '6';
+	}elseif (strtoupper($month) == 'JULY' || $month == 7){
+		return '7';
+	}elseif (strtoupper($month) == 'AUGUST' || $month == 8){
+		return '8';
+	}elseif (strtoupper($month) == 'SEPTEMBER' || $month == 9){
+		return '9';
+	}elseif (strtoupper($month) == 'OCTOBER' || $month == 10){
+		return '10';
+	}elseif (strtoupper($month) == 'NOVEMBER' || $month == 11){
+		return '11';
+	}elseif (strtoupper($month) == 'DECEMBER' || $month == 12){
+		return '12'; 
+	}else{
+		return $month;
+	}
+}
+function dateExplode($date){
+	$date=explode('-',$date);
+	$date=monthConvert($date[1])." ".$date[2].", ".$date[0];
+	return $date;
+}
+function addZero($num){
+	if (strlen($num) == 1){
+		return "0".$num;
+	}else{
+		return $num;
+	}
+}
+function dateImplode($date){
+	$str=explode(' AT ',$date);
+	$time=str_replace(' ','',$str[1]);
+	$date2=explode(' ',$str[0]);
+	$month=month2num(trim($date2[0]));
+	$day=str_replace(',','',$date2[1]);
+	$year=$date2[2];
+	return $year.'-'.addZero($month).'-'.addZero($day);
+}
+function postDateImplode($date){
+	$str=explode(' ',$date);
+	$time=$str[3].$str[4];
+	$month=month2num(trim($str[0]));
+	$day=str_replace(',','',$str[1]);
+	$year=$str[2];
+	return $year.'-'.addZero($month).'-'.addZero($day);
+}
+function mailExplode($histID){
+	$qh="SELECT action_str FROM ps_history WHERE history_id='$histID'";
+	$rh=@mysql_query($qh) or die (mysql_error());
+	$dh=mysql_fetch_array($rh,MYSQL_ASSOC);
+	if ($dh != ''){
+		$action=explode('.</LI>',strtoupper($dh[action_str]));
+		$dt=explode('ON ',$action[0]);
+		$count=count($dt)-1;
+		$return=postDateImplode(trim($dt["$count"]));
+	}
+	return $return;
+}
+function attemptExplode($packet,$defendant,$address,$type){
+	$qh="SELECT action_str, serverID FROM ps_history WHERE packet_id='$packet' AND defendant_id='$defendant' AND action_str LIKE '%$address%' AND WIZARD='$type' AND onAffidavit='checked'";
+	$rh=@mysql_query($qh) or die (mysql_error());
+	while ($dh=mysql_fetch_array($rh,MYSQL_ASSOC)){
+		if (isAddress(strtoupper($dh[action_str]),$address) != false){
+			$action=explode('</LI>',strtoupper($dh[action_str]));
+			$dt=explode('<BR>',$action[1]);
+			if ($type == 'POSTING DETAILS'){
+				$return[0]=postDateImplode($dt[0]);
+			}else{
+				$return[0]=dateImplode($dt[0]);
+			}
+			$return[1]=id2name($dh[serverID]);
+		}
+	}
+	return $return;
+}
+function deliveryExplode($histID){
+	$qh="SELECT action_str FROM ps_history WHERE history_id='$histID'";
+	$rh=@mysql_query($qh) or die (mysql_error());
+	$dh=mysql_fetch_array($rh,MYSQL_ASSOC);
+	if ($dh != ''){
+		$action=explode('DATE OF SERVICE: ',strtoupper($dh[action_str]));
+		$dt=explode('<BR>',$action[1]);
+		$return=dateImplode($dt[0]);
+	}
+	return $return;
+}
+function updateCO($co,$packet){
+	$q="SELECT closeOut, service_status FROM ps_packets WHERE packet_id='$packet'";
+	$r=@mysql_query($q) or die (mysql_error());
+	$d=mysql_fetch_array($r,MYSQL_ASSOC);
+	if ($d[service_status] != 'CANCELLED' && $d[service_status] != 'IN PROGRESS'){
+		if ($d[closeOut] == '0000-00-00'){
+			@mysql_query("UPDATE ps_packets SET closeOut='$co' WHERE packet_id='$packet'");
+			echo "<div style='border:2px solid red;'>$packet :: $co</div>";
+			hardLog('updating closeOut to '.$co.' For OTD'.$packet,'debug');
+		}else{
+			if ($d[closeOut] < $co){
+				@mysql_query("UPDATE ps_packets SET closeOut='$co' WHERE packet_id='$packet'");
+				echo "<div style='border:2px solid red;'>$packet :: $co</div>";
+				hardLog('updating closeOut to '.$co.' For OTD'.$packet,'debug');
+			}
+		}
+	}
+}
+$packet=3678;
+$q10a="SELECT * from ps_history WHERE packet_id > '$packet' AND actionDate='0000-00-00 00:00:00' ORDER BY history_id ASC";
+$r10a=@mysql_query($q10a) or die(mysql_error());
+while ($d10a=mysql_fetch_array($r10a, MYSQL_ASSOC)){
+	$closeOut='';
+	if ($d10a[wizard] == 'BORROWER' || $d10a[wizard] == 'NOT BORROWER'){
+		$dt = deliveryExplode($d10a[history_id]);
+		//@mysql_query(UPDATE ps_history SET actionDate='$dt' WHERE history_id='$d10a[history_id]') or die (mysql_error());
+		echo "<div>$d10a[history_id] :: OTD$d10a[packet_id] :: $dt</div>";
+	}elseif($d10a[action_type] == 'Attempted Service' || $d10a[wizard] == 'POSTING DETAILS'){
+		$closeOut = attemptExplode($d10a[history_id]);
+		//@mysql_query(UPDATE ps_history SET actionDate='$dt' WHERE history_id='$d10a[history_id]') or die (mysql_error());
+		echo "<div>$d10a[history_id] :: OTD$d10a[packet_id] :: $dt</div>";
+	}elseif($d10a[wizard] == 'MAILING DETAILS'){
+		$closeOut = mailExplode($d10a[history_id]);
+		//@mysql_query(UPDATE ps_history SET actionDate='$dt' WHERE history_id='$d10a[history_id]') or die (mysql_error());
+		echo "<div>$d10a[history_id] :: OTD$d10a[packet_id] :: $dt</div>";
+	}else{
+		echo "<div style='background-color:red;'>$d10a[history_id] :: OTD$d10a[packet_id] :: $d10a[wizard]</div>";
+	}
+}
+$q10b="SELECT * from evictionHistory WHERE actionDate='0000-00-00 00:00:00' ORDER BY history_id ASC";
+$r10b=@mysql_query($q10b) or die(mysql_error());
+while ($d10b=mysql_fetch_array($r10b, MYSQL_ASSOC)){
+	$closeOut='';
+	if ($d10b[wizard] == 'BORROWER' || $d10b[wizard] == 'NOT BORROWER'){
+		$dt = deliveryExplode($d10b[history_id]);
+		//@mysql_query(UPDATE evictionHistory SET actionDate='$dt' WHERE history_id='$d10b[history_id]') or die (mysql_error());
+		echo "<div>$d10b[history_id] :: EV$d10b[eviction_id] :: $dt</div>";
+	}elseif($d10b[action_type] == 'Attempted Service' || $d10b[wizard] == 'POSTING DETAILS'){
+		$closeOut = attemptExplode($d10b[history_id]);
+		//@mysql_query(UPDATE evictionHistory SET actionDate='$dt' WHERE history_id='$d10b[history_id]') or die (mysql_error());
+		echo "<div>$d10b[history_id] :: EV$d10b[eviction_id] :: $dt</div>";
+	}elseif($d10b[wizard] == 'MAILING DETAILS'){
+		$closeOut = mailExplode($d10b[history_id]);
+		//@mysql_query(UPDATE evictionHistory SET actionDate='$dt' WHERE history_id='$d10b[history_id]') or die (mysql_error());
+		echo "<div>$d10b[history_id] :: EV$d10b[eviction_id] :: $dt</div>";
+	}else{
+		echo "<div style='background-color:red;'>$d10b[history_id] :: EV$d10b[eviction_id] :: $d10b[wizard]</div>";
+	}
+}
+
+?>
