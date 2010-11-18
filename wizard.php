fcecf8b717ad159c7c9b1a851eb808f954ee662e
zsalwen <zach@mdwestserve.com>
2010-11-18 09:53:53 -0800

    echoing authSeller value

--- wizard.php
+++ wizard.php
@@ -127,6 +127,7 @@ $template = str_replace('[today]',date('F jS, Y'),$template);
 $template = str_replace('[ps_plaintiff]',str_replace('-','<br>',strtoupper($attorney[ps_plaintiff])),$template);
 $template = str_replace('[attName]',strtoupper($attorney[full_name]),$template);
 $template = str_replace('[authSeller]',strtoupper($attorney[authSeller]),$template);
+echo "<div>AUTH SELLER: $attorney[authSeller]</div>";
 $template = str_replace('[attAddress]',str_replace(' -',',',$attorney[justAddress]),$template);
 $template = str_replace('[attPhone]',$attorney[phone],$template);
 
