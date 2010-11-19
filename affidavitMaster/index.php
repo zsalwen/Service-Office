9c2bffe3bd737a42502272ffc86a543bfc6ef537
zsalwen <zach@mdwestserve.com>
2010-11-19 07:19:52 -0800

    Reverting to header redirect

--- affidavitMaster/index.php
+++ affidavitMaster/index.php
@@ -37,11 +37,10 @@ fwrite($fh, $d[LiveAffidavit]);
 fclose($fh);
 $command = 'python DocumentConverter.py /gitbox/Service-Office/affidavitMaster/'.$id.'.html /gitbox/Service-Office/affidavitMaster/'.$id.'.pdf';
 $error = system($command,$result);
-echo "<div>".$command."</div>";
-echo "<div>".$error."</div>";
-echo "<div>".$result."</div>";
-echo "<script>window.open($id.".".$pdf,'PDF')</script>";
-//header('Location: '.$id.'.pdf');
+//echo "<div>".$command."</div>";
+//echo "<div>".$error."</div>";
+//echo "<div>".$result."</div>";
+header('Location: '.$id.'.pdf');
 }
 
 /*
