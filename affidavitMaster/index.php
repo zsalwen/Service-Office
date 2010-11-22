a00aa67e2a90113baa3cea767e247931d714e054
zsalwen <zach@mdwestserve.com>
2010-11-22 12:33:24 -0800

    Trying to flush through PCL before converting to PDF

--- affidavitMaster/index.php
+++ affidavitMaster/index.php
@@ -24,89 +24,105 @@ $myFile = "$id.html";
 $fh = fopen($myFile, 'w') or die("can't open file");
 fwrite($fh, $d[LiveAdHTML]);
 fclose($fh);
-$error = system('python DocumentConverter.py /gitbox/Service-Office/'.$id.'.html /gitbox/Service-Office/'.$id.'.doc',$result);
+$error = system('python DocumentConverter.py /sandbox/CORE/'.$id.'.html /sandbox/CORE/'.$id.'.doc',$result);
 header('Location: '.$id.'.doc');
 }
 */
 function pdfAD($id){
-$r=@mysql_query("select LiveAffidavit from ps_packets where packet_id = '$id'");
-$d=mysql_fetch_array($r,MYSQL_ASSOC);
-$myFile = "$id.html";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$LiveAffidavit=str_replace('page-break-after','page-break-before',$d[LiveAffidavit]);
-fwrite($fh, $LiveAffidavit);
-fclose($fh);
-$command = 'python DocumentConverter.py /gitbox/Service-Office/affidavitMaster/'.$id.'.html /gitbox/Service-Office/affidavitMaster/'.$id.'.pdf';
-$error = system($command,$result);
-//echo "<div>".$command."</div>";
-//echo "<div>".$error."</div>";
-//echo "<div>".$result."</div>";
-header('Location: '.$id.'.pdf');
+	$r=@mysql_query("select LiveAffidavit from ps_packets where packet_id = '$id'");
+	$d=mysql_fetch_array($r,MYSQL_ASSOC);
+	$myFile = "$id.html";
+	$fh = fopen($myFile, 'w') or die("can't open file");
+	fwrite($fh, $d[LiveAffidavit]);
+	fclose($fh);
+	$command = 'python DocumentConverter.py /sandbox/staff/affidavitMaster/'.$id.'.html /sandbox/staff/affidavitMaster/'.$id.'.pdf';
+	$error = system($command,$result);
+	//echo "<div>".$command."</div>";
+	//echo "<div>".$error."</div>";
+	//echo "<div>".$result."</div>";
+	header('Location: '.$id.'.pdf');
 }
-
+function pdfAD2($id){
+	$r=@mysql_query("select LiveAffidavit from ps_packets where packet_id = '$id'");
+	$d=mysql_fetch_array($r,MYSQL_ASSOC);
+	$myFile = "$id.html";
+	$fh = fopen($myFile, 'w') or die("can't open file");
+	fwrite($fh, $d[LiveAffidavit]);
+	fclose($fh);
+	passthru('/usr/local/bin/html2ps '.$id.'.html > '.$id.'.pcl');
+	$file = $id.'.pcl';
+	passthru('/usr/local/bin/html2ps '.$id.'.pcl > '.$id.'.html');
+	$command = 'python DocumentConverter.py /sandbox/staff/affidavitMaster/'.$id.'.html /sandbox/staff/affidavitMaster/'.$id.'.pdf';
+	$error = system($command,$result);
+	//echo "<div>".$command."</div>";
+	//echo "<div>".$error."</div>";
+	//echo "<div>".$result."</div>";
+	header('Location: '.$id.'.pdf');
+}
 /*
 if($_GET['doc']){
 	docAD($_GET[id]);
 }
 */
 if($_GET['pdf']){
-	pdfAD($_GET[id]);
+	pdfAD2($_GET[id]);
 }
+
 
 function printAD($id,$ip){
-$r=@mysql_query("select LiveAffidavit from ps_packets where packet_id = '$id'");
-$d=mysql_fetch_array($r,MYSQL_ASSOC);
-$myFile = "$id.html";
-$fh = fopen($myFile, 'w') or die("can't open file");
-fwrite($fh, $d[LiveAffidavit]);
-fclose($fh);
-passthru('/usr/local/bin/html2ps '.$id.'.html > '.$id.'.pcl');
-$file = $id.'.pcl';
-$remote_file = $id.'.pcl';
+	$r=@mysql_query("select LiveAffidavit from ps_packets where packet_id = '$id'");
+	$d=mysql_fetch_array($r,MYSQL_ASSOC);
+	$myFile = "$id.html";
+	$fh = fopen($myFile, 'w') or die("can't open file");
+	fwrite($fh, $d[LiveAffidavit]);
+	fclose($fh);
+	passthru('/usr/local/bin/html2ps '.$id.'.html > '.$id.'.pcl');
+	$file = $id.'.pcl';
+	$remote_file = $id.'.pcl';
 
 
 
 
-if ($conn_id = ftp_connect($ip)) {
-	//echo "Current directory is now: " . ftp_pwd($conn_id) . "\n";
-} else { 
-	//echo "Couldn't change directory\n";
-	mail('insidenothing@gmail.com','Affidavit Master: PRINTER FAILED TO CONNECT','Couldn\'t connect');
-	error_log(date('r')." Affidavit Master: $ip WARNING: Couldn't connect. \n", 3, '/logs/printer.log');
-	return 'fail';
-}
-
-
-
-
-
-$login_result = ftp_login($conn_id, 'alpha', 'beta');
+	if ($conn_id = ftp_connect($ip)) {
+		//echo "Current directory is now: " . ftp_pwd($conn_id) . "\n";
+	} else { 
+		//echo "Couldn't change directory\n";
+		mail('insidenothing@gmail.com','Affidavit Master: PRINTER FAILED TO CONNECT','Couldn\'t connect');
+		error_log(date('r')." Affidavit Master: $ip WARNING: Couldn't connect. \n", 3, '/logs/printer.log');
+		return 'fail';
+	}
 
 
 
 
 
-ftp_pasv($conn_id, true);
-if (ftp_chdir($conn_id, "PORT1")) {
-	//echo "Current directory is now: " . ftp_pwd($conn_id) . "\n";
-} else { 
-	//echo "Couldn't change directory\n";
-	mail('insidenothing@gmail.com','Affidavit Master: PRINTER CHDIR','Couldn\'t change directory');
-	error_log(date('r')." Affidavit Master: $ip WARNING: Couldn't change ftp directory - $id. \n", 3, '/logs/printer.log');
-}
-if (ftp_put($conn_id, $remote_file, $file, FTP_BINARY)) {
-	//echo "successfully uploaded $file\n";
-	$last_line = system('rm -f '.$id.'.pcl', $retval);
-	$last_line = system('rm -f '.$id.'.rtf', $retval);
-	$last_line = system('rm -f '.$id.'.html', $retval);
-	error_log(date('r')." $ip NOTICE: Burson edited ad $id printed successfully. \n", 3, '/logs/printer.log');
-} else {
-	//echo "There was a problem while uploading $file\n";
-	mail('insidenothing@gmail.com','Affidavit Master: AI Break: FTP PUT','There was a problem while uploading '.$file);
-	error_log(date('r')." Affidavit Master: $ip ERROR: There was a problem while uploading - $id. \n", 3, '/logs/printer.log');
-	return 'fail';
-}
-ftp_close($conn_id);
+	$login_result = ftp_login($conn_id, 'alpha', 'beta');
+
+
+
+
+
+	ftp_pasv($conn_id, true);
+	if (ftp_chdir($conn_id, "PORT1")) {
+		//echo "Current directory is now: " . ftp_pwd($conn_id) . "\n";
+	} else { 
+		//echo "Couldn't change directory\n";
+		mail('insidenothing@gmail.com','Affidavit Master: PRINTER CHDIR','Couldn\'t change directory');
+		error_log(date('r')." Affidavit Master: $ip WARNING: Couldn't change ftp directory - $id. \n", 3, '/logs/printer.log');
+	}
+	if (ftp_put($conn_id, $remote_file, $file, FTP_BINARY)) {
+		//echo "successfully uploaded $file\n";
+		$last_line = system('rm -f '.$id.'.pcl', $retval);
+		$last_line = system('rm -f '.$id.'.rtf', $retval);
+		$last_line = system('rm -f '.$id.'.html', $retval);
+		error_log(date('r')." $ip NOTICE: Burson edited ad $id printed successfully. \n", 3, '/logs/printer.log');
+	} else {
+		//echo "There was a problem while uploading $file\n";
+		mail('insidenothing@gmail.com','Affidavit Master: AI Break: FTP PUT','There was a problem while uploading '.$file);
+		error_log(date('r')." Affidavit Master: $ip ERROR: There was a problem while uploading - $id. \n", 3, '/logs/printer.log');
+		return 'fail';
+	}
+	ftp_close($conn_id);
 }
 
 if ($_GET[id]){
