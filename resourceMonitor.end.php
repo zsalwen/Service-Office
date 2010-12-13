c5f1fc26f96ddc7c040ac49ba144937ff64a5786
Patrick McGuire <insidenothing@gmail.com>
2010-12-13 09:27:48 -0800

    only > 1

--- resourceMonitor.end.php
+++ resourceMonitor.end.php
@@ -4,6 +4,8 @@ $mtimeResourceMonitorStart = explode (" ", $mtimeResourceMonitorStart);
 $mtimeResourceMonitorStart = $mtimeResourceMonitorStart[1] + $mtimeResourceMonitorStart[0];
 $tendResourceMonitorStart = $mtimeResourceMonitorStart;
 $totaltimeResourceMonitorStart = ($tendResourceMonitorStart - $tstartResourceMonitorStart);
+if ($totaltimeResourceMonitorStart > '1'){
 $logResourceMonitorStart = '/logs/response.log';
+}
 resourceMonitorStartServerResponse(str_replace('/sandbox/','',$_SERVER["SCRIPT_FILENAME"]),number_format($totaltimeResourceMonitorStart,10),$_SERVER["QUERY_STRING"],$logResourceMonitorStart,$_GET[debug]);
 ?>
