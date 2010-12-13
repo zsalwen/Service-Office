959a41eab785739e6e4a3b12095a944b3bfead87
Patrick McGuire <insidenothing@gmail.com>
2010-12-13 08:46:50 -0800

    only one response log

--- resourceMonitor.end.php
+++ resourceMonitor.end.php
@@ -4,10 +4,6 @@ $mtimeResourceMonitorStart = explode (" ", $mtimeResourceMonitorStart);
 $mtimeResourceMonitorStart = $mtimeResourceMonitorStart[1] + $mtimeResourceMonitorStart[0];
 $tendResourceMonitorStart = $mtimeResourceMonitorStart;
 $totaltimeResourceMonitorStart = ($tendResourceMonitorStart - $tstartResourceMonitorStart);
-if ($totaltimeResourceMonitorStart < '60'){
 $logResourceMonitorStart = '/logs/response.log';
-}else{
-$logResourceMonitorStart = '/logs/slow.response.log';
-}
 resourceMonitorStartServerResponse(str_replace('/sandbox/','',$_SERVER["SCRIPT_FILENAME"]),number_format($totaltimeResourceMonitorStart,10),$_SERVER["QUERY_STRING"],$logResourceMonitorStart,$_GET[debug]);
 ?>
