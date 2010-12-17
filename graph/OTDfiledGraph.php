37614530b0fbd05c864214dd6ad766dc8b876067
zsalwen <zach@mdwestserve.com>
2010-12-17 13:23:02 -0600

    Updating graphs to dynamically display from 2008-current

--- graph/OTDfiledGraph.php
+++ graph/OTDfiledGraph.php
@@ -45,7 +45,6 @@ while ($year <= $curYear){
 //pull BURSON files
 $year=2008;
 while ($year <= $curYear){
-	$yr=substr($year,-2);
 	if ($year != $curYear){
 		$topMo=12;
 	}else{
@@ -74,7 +73,6 @@ while ($year <= $curYear){
 //pull WHITE files
 $year=2008;
 while ($year <= $curYear){
-	$yr=substr($year,-2);
 	if ($year != $curYear){
 		$topMo=12;
 	}else{
@@ -103,7 +101,6 @@ while ($year <= $curYear){
 //pull BGW files
 $year=2008;
 while ($year <= $curYear){
-	$yr=substr($year,-2);
 	if ($year != $curYear){
 		$topMo=12;
 	}else{
@@ -132,7 +129,6 @@ while ($year <= $curYear){
 //pull OTHER files
 $year=2008;
 while ($year <= $curYear){
-	$yr=substr($year,-2);
 	if ($year != $curYear){
 		$topMo=12;
 	}else{
@@ -170,7 +166,7 @@ echo "<tr><td>WHITE:</td><td>".str_replace(',','</td><td>',$white).'</td></tr>';
 echo "<tr><td>BGW:</td><td>".str_replace(',','</td><td>',$bgw).'</td></tr>';
 echo "<tr><td>OTHER:</td><td>".str_replace(',','</td><td>',$other).'</td>';
 echo "</tr></table>";*/
-$src="http://2.chart.apis.google.com/chart?cht=lc&chs=900x333&chd=t:".$src."|".$burson."|".$white."|".$bgw."|".$other."&chxl=0:".$src2."|1:|0|$z1|$z2|$z3|$z4|$z&chtt=Foreclosure File Dates 2008-2009&chdl=All Files|Burson|White|BGW|Others&chco=FF0000,00FF00,0000FF,6622AA,FF7700";
+$src="http://2.chart.apis.google.com/chart?cht=lc&chs=900x333&chd=t:".$src."|".$burson."|".$white."|".$bgw."|".$other."&chxl=0:".$src2."|1:|0|$z1|$z2|$z3|$z4|$z&chtt=Foreclosure File Dates 2008-$curYear&chdl=All Files|Burson|White|BGW|Others&chco=FF0000,00FF00,0000FF,6622AA,FF7700";
 $markers="&chm=d,990000,0,-1,5|d,009900,1,-1,5|d,000099,2,-1,5|d,662266,3,-1,5|d,994400,4,-1,5|t$z$zzz,000000,0,$zz,10";
 $rest="&chxt=x,y&chds=0,".$z."&chxtc=0,10|1,-980&chxs=0,000000,7|1,000000,10,-1,lt,333333";
 ?>
