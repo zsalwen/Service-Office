174014cdb3ddb238613afe55797955b33d12e2b3
zsalwen <zach@mdwestserve.com>
2010-12-20 16:42:03 -0500

    Sizing iframes to exact dimensions of graphs

--- graph/index.php
+++ graph/index.php
@@ -5,22 +5,22 @@ include "functions.php";
 <style>
 td {text-align:center;}
 </style>
-<table align="center" width='100%'>
+<table align="center" width='80%'>
 
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDcostGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/OTDcostGraph.php"></iframe></td>
 </tr>
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDreceivedGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/OTDreceivedGraph.php"></iframe></td>
 </tr>
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDfiledGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/OTDfiledGraph.php"></iframe></td>
 </tr>
 
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDexportGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/OTDexportGraph.php"></iframe></td>
 </tr>
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVreceivedGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/EVreceivedGraph.php"></iframe></td>
 </tr>
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVfiledGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/EVfiledGraph.php"></iframe></td>
 </tr>
-<tr><td height='100%'><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVexportGraph.php"></iframe></td>
+<tr><td><iframe width='1000' height='400' src="http://staff.mdwestserve.com/graph/EVexportGraph.php"></iframe></td>
 </tr>
 
 <tr><td><img src="http://staff.mdwestserve.com/graph/cost.php?year=2008&attid=<?=$_GET[attid];?>"></td>
