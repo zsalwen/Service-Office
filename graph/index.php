9a6c814c0ea173cc70add67fcc7f05e576db4808
zsalwen <zach@mdwestserve.com>
2010-12-20 16:25:09 -0500

    Trying height 100%

--- graph/index.php
+++ graph/index.php
@@ -1,24 +1,26 @@
 <?
 include "functions.php";
 ?>
-
 <link rel="stylesheet" type="text/css" href="../fire.css" />
+<style>
+td {text-align:center;}
+</style>
 <table align="center" width='100%' height='100%'>
 
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/OTDcostGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDcostGraph.php"></iframe></td>
 </tr>
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/OTDreceivedGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDreceivedGraph.php"></iframe></td>
 </tr>
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/OTDfiledGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDfiledGraph.php"></iframe></td>
 </tr>
 
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/OTDexportGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/OTDexportGraph.php"></iframe></td>
 </tr>
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/EVreceivedGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVreceivedGraph.php"></iframe></td>
 </tr>
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/EVfiledGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVfiledGraph.php"></iframe></td>
 </tr>
-<tr><td><iframe width='100%' height='50%' src="http://staff.mdwestserve.com/graph/EVexportGraph.php"></iframe></td>
+<tr><td><iframe width='100%' height='100%' src="http://staff.mdwestserve.com/graph/EVexportGraph.php"></iframe></td>
 </tr>
 
 <tr><td><img src="http://staff.mdwestserve.com/graph/cost.php?year=2008&attid=<?=$_GET[attid];?>"></td>
