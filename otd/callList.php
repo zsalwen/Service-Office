cead909818cb85790811e9fe8847ed46173a7b96
zsalwen <zach@mdwestserve.com>
2010-12-21 16:45:25 -0500

    Adding link to contractor_profile.php, displaying service status, using reopenDate as dispatch date if reopenDate > dispatchDate

--- otd/callList.php
+++ otd/callList.php
@@ -15,14 +15,18 @@ function oosList($packet){
 	$r=@mysql_query($q) or die ("Query: $q<br>".mysql_error());
 	$d=mysql_fetch_array($r,MYSQL_ASSOC);
 	if ($d[state1] != '' && strtoupper($d[state1]) != 'MD' && $d[server_id] != '' && id2state($d[server_id]) != 'MD' && $d[server_id] != 218){$i++;
-		$list .= $i.". ".id2name($d[server_id]).": <div style='font-size:10px; text-align:right !important; line-height:10px; border:solid 1px black;'>".strtoupper($d[address1])."<br>".strtoupper($d[city1]).", ".strtoupper($d[state1])." ".$d[zip1]."</div>";
+		$list .= $i.". <a href='http://staff.mdwestserve.com/contractor_profile.php?admin=$d[server_id]' target='_blank'>".id2name($d[server_id])."</a>: <div style='font-size:10px; text-align:right !important; line-height:10px; border:solid 1px black;'>".strtoupper($d[address1])."<br>".strtoupper($d[city1]).", ".strtoupper($d[state1])." ".$d[zip1]."</div>";
 	}
 	foreach(range('a','e') as $letter){
 		if ($d["state1$letter"] != '' && strtoupper($d["state1$letter"]) != 'MD' && $d["server_id$letter"] != '' && id2state($d["server_id$letter"]) != 'MD' && $d["server_id$letter"] != 218){$i++;
-			$list .= $i.". ".id2name($d["server_id$letter"]).": <div style='font-size:10px; text-align:right !important; line-height:10px; border:solid 1px black;'>".strtoupper($d["address1$letter"])."<br>".strtoupper($d["city1$letter"]).", ".strtoupper($d["state1$letter"])." ".$d["zip1$letter"]."</div>";
+			$list .= $i.". <a href='http://staff.mdwestserve.com/contractor_profile.php?admin=".$d["server_id$letter"]."' target='_blank'>".id2name($d["server_id$letter"])."</a>: <div style='font-size:10px; text-align:right !important; line-height:10px; border:solid 1px black;'>".strtoupper($d["address1$letter"])."<br>".strtoupper($d["city1$letter"]).", ".strtoupper($d["state1$letter"])." ".$d["zip1$letter"]."</div>";
 		}
 	}
 	return $list;
+}
+function justDate($dt){
+	$date=explode(' ',$dt);
+	return $date[0];
 }
 $today=date('m/d/y');
 $limbo=time()-86400;
@@ -50,15 +54,27 @@ function ChangeText(field){
 </script>
 <?
 echo "<table align='center' border='1' style='border-collapse:collapse;'><tr><td align='center' colspan='4' style='font-size:16px;'>OUT OF STATE SERVER CALL LIST</td></tr>
-<tr><td align='center'>Packet #</td><td align='center'>Dispatch Date</td><td align='center'>Est. File Date</td><td align='center' style='padding-left:20px;'>Servers to Call</td><td align='center'>Notes</td></tr>";
+<tr><td align='center'>Packet #</td><td align='center'>Dispatch Date</td><td align='center'>Service Status</td><td align='center'>Est. File Date</td><td align='center' style='padding-left:20px;'>Servers to Call</td><td align='center'>Notes</td></tr>";
 $q="SELECT * from ps_packets WHERE (affidavit_status = 'SERVICE CONFIRMED' OR affidavit_status='ASSIGNED') and  filing_status <> 'FILED WITH COURT' AND filing_status <> 'FILED WITH COURT - FBS' AND status <> 'CANCELLED' AND filing_status <> 'FILED BY CLIENT' AND filing_status <> 'REQUESTED-DO NOT FILE!' AND filing_status <> 'SEND TO CLIENT' AND status <> 'DUPLICATE' AND status <> 'FILE COPY' AND service_status <> 'MAIL ONLY' AND ((state1 <> '' AND state1 <> 'md' AND state1 <> 'MD') OR (state1a <> '' AND state1a <> 'md' AND state1a <> 'MD') OR (state1b <> '' AND state1b <> 'md' AND state1b <> 'MD') OR (state1c <> '' AND state1c <> 'md' AND state1c <> 'MD') OR (state1d <> '' AND state1d <> 'md' AND state1d <> 'MD') OR (state1e <> '' AND state1e <> 'md' AND state1e <> 'MD')) AND processor_notes NOT LIKE '%$today%' AND dispatchDate <= '$limbo' ORDER BY packet_id ASC";
 $r=@mysql_query($q) or die ("Query: $q<br>".mysql_error());
 $i=0;
 while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){$i++;
 	$oosList=oosList($d[packet_id]);
 	//if ($oosList != ''){
+		if ($d[reopenDate] >= justDate($d[dispatchDate])){
+			$dispatchDate=$d[reopenDate]."-REOPENED";
+		}else{
+			$dispatchDate=$d[dispatchDate];
+		}
+		if ($d[affidavit_status] == 'ASSIGNED'){
+			$status='ACTIVE';
+			$color='green';
+		}else{
+			$status='BLACKHOLE';
+			$color='yellow';
+		}
 		echo "
-		<tr bgcolor='".row_color($i,'#FFFFFF','#DDDDDD')."'><td valign='top'><a href='order.php?packet=$d[packet_id]' target='_blank'>$d[packet_id]</a></td><td valign='top'>$d[dispatchDate]</td><td valign='top'>$d[estFileDate]</td><td style='padding-left:20px;' valign='top'>".$oosList."</td><td width='600px' valign='top'><span style='background-color:orange; display:inline;' onClick=\"hideshow(document.getElementById('notes-$d[packet_id]')); ChangeText('plus-$d[packet_id]');\">[<a id='plus-$d[packet_id]'>+</a>]</span><div style='display:none;' id='notes-$d[packet_id]'><iframe height='300px' width='600px' frameborder='0' src='http://staff.mdwestserve.com/notes.php?packet=$d[packet_id]'></iframe></div></td></tr>";
+		<tr bgcolor='".row_color($i,'#FFFFFF','#DDDDDD')."'><td valign='top'><a href='order.php?packet=$d[packet_id]' target='_blank'>$d[packet_id]</a></td><td valign='top'>$dispatchDate</td><td valign='top' style='background-color:$color'>$status</td><td valign='top'>$d[estFileDate]</td><td style='padding-left:20px;' valign='top'>".$oosList."</td><td width='600px' valign='top'><span style='background-color:orange; display:inline;' onClick=\"hideshow(document.getElementById('notes-$d[packet_id]')); ChangeText('plus-$d[packet_id]');\">[<a id='plus-$d[packet_id]'>+</a>]</span><div style='display:none;' id='notes-$d[packet_id]'><iframe height='300px' width='600px' frameborder='0' src='http://staff.mdwestserve.com/notes.php?packet=$d[packet_id]'></iframe></div></td></tr>";
 	//}
 }
 echo "</table>";
