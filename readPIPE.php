51bab6b194a0a928252e59bfaca0aa64802e95b1
Patrick McGuire <insidenothing@gmail.com>
2011-01-02 17:58:34 -0800

    base

---
+++ readPIPE.php
@@ -0,0 +1,20 @@
+<php
+$pipe="fifo";
+
+if (! $handle=@fopen($pipe,"r") ) {
+   echo "can't open pipe for reading\n";
+   exit;
+}
+
+
+while (1) {
+
+    $bytes=fread($handle,1);
+    if ( strlen($bytes) == 0 ) 
+       break;
+
+    echo $bytes;
+}
+
+fclose($handle);
+?>
