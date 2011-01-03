86197334caf26dbe8341af36d36f46e3e18d2f08
Patrick McGuire <insidenothing@gmail.com>
2011-01-02 18:37:00 -0800

    read old pipe?

--- readPIPE.php
+++ readPIPE.php
@@ -2,15 +2,18 @@
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [start] \n", 3, '/logs/fail.log');
   $pipe="/tmp/pipe";
    if(!file_exists($pipe)) {
-error_log("[".date('g:iA n/j/y')."] [readPIPE] [stale pipe] \n", 3, '/logs/fail.log');
-      echo "I am not blocked!";
+      echo "<small>stale pipe</small>";
+      $f = fopen($pipe,"r");
+$data = fread($f,10);
+error_log("[".date('g:iA n/j/y')."] [readPIPE] [stale pipe] [$data] \n", 3, '/logs/fail.log');
    }
    else {
       //block and read from the pipe
+echo "<small>live pipe</small>";
       $f = fopen($pipe,"r");
 $data = fread($f,10);
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [live pipe] [$data] \n", 3, '/logs/fail.log');
-      echo $data;
    }
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [end] \n", 3, '/logs/fail.log');
+      echo $data;
 ?>
