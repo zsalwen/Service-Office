68e17303fdafe91da4bba974c7a66b7327b2c9c9
Patrick McGuire <insidenothing@gmail.com>
2011-01-02 18:24:58 -0800

    new code

--- readPIPE.php
+++ readPIPE.php
@@ -1,26 +1,13 @@
 <?php
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [start] \n", 3, '/logs/fail.log');
-$pipe="/data/fifo";
-
-
-
-if (! $handle=fopen($pipe,"r") ) {
-   error_log("[".date('g:iA n/j/y')."] [readPIPE] [fopen failed] \n", 3, '/logs/fail.log');
-   echo "can't open pipe for reading\n";
-   exit;
-}
-
-
-while (1) {
-
-    $bytes=fread($handle,1);
-    if ( strlen($bytes) == 0 ) 
-       break;
-
-    echo $bytes;
-}
-
-fclose($handle);
+  $pipe="/tmp/pipe";
+   if(!file_exists($pipe)) {
+      echo "I am not blocked!";
+   }
+   else {
+      //block and read from the pipe
+      $f = fopen($pipe,"r");
+      echo fread($f,10);
+   }
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [end] \n", 3, '/logs/fail.log');
-
 ?>
