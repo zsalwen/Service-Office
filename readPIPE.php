2f995412b0f3fe9453a97c759c4d87faf33a7270
Patrick McGuire <insidenothing@gmail.com>
2011-01-02 18:31:35 -0800

    better log reader

--- readPIPE.php
+++ readPIPE.php
@@ -2,12 +2,15 @@
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [start] \n", 3, '/logs/fail.log');
   $pipe="/tmp/pipe";
    if(!file_exists($pipe)) {
+error_log("[".date('g:iA n/j/y')."] [readPIPE] [stale pipe] \n", 3, '/logs/fail.log');
       echo "I am not blocked!";
    }
    else {
       //block and read from the pipe
       $f = fopen($pipe,"r");
-      echo fread($f,10);
+$data = fread($f,10);
+error_log("[".date('g:iA n/j/y')."] [readPIPE] [live pipe] [$data] \n", 3, '/logs/fail.log');
+      echo $data;
    }
 error_log("[".date('g:iA n/j/y')."] [readPIPE] [end] \n", 3, '/logs/fail.log');
 ?>
