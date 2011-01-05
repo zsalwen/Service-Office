9f5e93f189c760d1260c3a660bd9d5bcdcecf179
zsalwen <zach@mdwestserve.com>
2011-01-05 13:44:44 -0500

    (

--- mailman.php
+++ mailman.php
@@ -129,7 +129,7 @@ function getPacketData($packet){
 		$data .= "<li><a href='http://staff.mdwestserve.com/otd/stuffPacket.2.php?packet=$packet&mail=1' target='_blank'><span style='color:green; background-color:black; font-weight:bold;'>GREEN HB472 Envelopes</span></a></li>";
 	}elseif($packet >= 12435 && $d[lossMit] != "N/A - OLD L" && $d[attorneys_id] == 70){
 		$data .= "<li><a href='http://staff.mdwestserve.com/otd/stuffPacket.bgw.php?packet=$packet&mail=1' target='_blank'><span style='color:white; background-color:black; font-weight:bold;'>WHITE HB472 Envelopes</span></a></li>";
-	}elseif$packet >= 12435 && $d[lossMit] != "N/A - OLD L"){
+	}elseif($packet >= 12435 && $d[lossMit] != "N/A - OLD L"){
 		$data .= "<li><a href='http://staff.mdwestserve.com/otd/stuffPacket.2.php?packet=$packet&mail=1' target='_blank'><span style='color:green; background-color:black; font-weight:bold;'>WHITE HB472 Envelopes</span></a></li>";
 	}
 	$data .= "</td><td nowrap class='noprint' style='background-color:#FF0000;'>";
