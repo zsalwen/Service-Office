47a3f173f3a7058e0ae5a38685075a0fff1ed030
zsalwen <zach@mdwestserve.com>
2011-01-07 15:06:26 -0500

    Trying return address one size smaller

--- envelopecard.bursonhb472.jpg.php
+++ envelopecard.bursonhb472.jpg.php
@@ -17,6 +17,7 @@ $font5 = "/fonts/arial.ttf";
 $font6 = "/fonts/arialbd.ttf";
 $size = "13";
 $size2 = "15";
+$size3 = "12";
 $notice1 = "REQUEST FOR FORECLOSURE MEDIATION";
 
 $line1="";
@@ -26,9 +27,9 @@ $return1 = "Shapiro & Burson LLP";
 $return2 = "13135 Lee Jackson Hwy #201"; 
 $return3 = "Fairfax, VA 22033"; 
 
-imageTTFText( $canvas, $size, 270, 360, 0, $black, $font, $return1);
-imageTTFText( $canvas, $size, 270, 340, 0, $black, $font, $return2);
-imageTTFText( $canvas, $size, 270, 320, 0, $black, $font, $return3);
+imageTTFText( $canvas, $size3, 270, 360, 0, $black, $font, $return1);
+imageTTFText( $canvas, $size3, 270, 340, 0, $black, $font, $return2);
+imageTTFText( $canvas, $size3, 270, 320, 0, $black, $font, $return3);
 $cord = "$clientFile".'X';
 //main label
 if (stripos($_GET[line1],"-")){
