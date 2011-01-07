4538965991316fe86f5af8c5b214fd6cad5a4ab5
zsalwen <zach@mdwestserve.com>
2011-01-07 13:59:11 -0500

    If $_GET[sb] use envelopecard.bursonhb472.jpg.php

--- otd/stuffPacket.2.php
+++ otd/stuffPacket.2.php
@@ -44,7 +44,7 @@ function envPrint($id,$times,$lossMit){
 		?>
 		<table style='page-break-after:always' align='center'><tr><td>
 		<div style='width:400px;height:40px;'></div>
-		<? if ($d[to1] == "SHAPIRO AND BURSON LLP"){ ?>
+		<? if ($_GET[sb]){ ?>
 		<img  src="http://staff.mdwestserve.com/envelopecard.bursonhb472.jpg.php?line1=<?=washAdd($d[to1])?>&line2=<?=washAdd($d[to2])?>&csz=<?=washAdd($d[to3])?>&client=<?=$client?>&lossMit=<?=$lossMit?>">
 		<? }else{ ?>
 		<img  src="http://staff.mdwestserve.com/envelopecard.hb472.jpg.php?line1=<?=washAdd($d[to1])?>&line2=<?=washAdd($d[to2])?>&csz=<?=washAdd($d[to3])?>&client=<?=$client?>&lossMit=<?=$lossMit?>">
