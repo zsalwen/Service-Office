8f9737dbca88431bc0dbe97874ef0c81929d4a9f
zsalwen <zach@mdwestserve.com>
2011-01-07 15:17:35 -0500

    Undoing, as the rates do not seem to have changed after all

--- whitecard.jpg.php
+++ whitecard.jpg.php
@@ -5,11 +5,11 @@ $white = imagecolorallocate( $canvas, 255, 255, 255 );
 $black = imagecolorallocate( $canvas, 0, 0, 0 );
 $font = "/fonts/verdana.ttf";
 $size = "12";
-$total = number_format($_GET[cost]+2.8+2.35,2);
+$total = number_format($_GET[cost]+2.8+2.3,2);
 if (!$_GET[noCost]){
 imageTTFText( $canvas, $size, 270, 195, 85, $black, $font, '$'.$_GET[cost] );
 imageTTFText( $canvas, $size, 270, 170, 85, $black, $font, '$2.80' );
-imageTTFText( $canvas, $size, 270, 145, 85, $black, $font, '$2.35' );
+imageTTFText( $canvas, $size, 270, 145, 85, $black, $font, '$2.30' );
 imageTTFText( $canvas, $size, 270, 120, 85, $black, $font, '$'.$total );
 }
 //imageTTFText( $canvas, $size, 270, 100, 85, $black, $font, '$4.90' );
