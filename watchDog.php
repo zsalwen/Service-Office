8ffa3fbe81c56bf0ea08f2c310be692e547c9bdd
zsalwen <zach@mdwestserve.com>
2011-01-28 10:43:49 -0500

    Using ob to log display only errors, enter into watchdog.log if they exist

--- watchDog.php
+++ watchDog.php
@@ -33,28 +33,32 @@ function casesOnline($first,$last,$county,$company){
 		return "0";
 	}
 }
+ob_start();
 mysql_connect();
 mysql_select_db('core');
 // first we have to build the search table ?? manual for now...
 
 // loop through watchDog table
-$rOut = @mysql_query("select * from watchDog where status <> 'Search Complete'");
+$rOut = @mysql_query("select * from watchDog where status <> 'Search Complete'") or die (mysql_error());
 while ($dOut=mysql_fetch_array($rOut,MYSQL_ASSOC)){
 	$id = $dOut['watchID'];
 	$response = casesOnline($dOut['firstName'],$dOut['lastName'],$dOut['county'],$dOut['company']);
 	//echo "<li>$dOut[firstName], $dOut[lastName], $dOut[county]: $response</li>";
 	if (!$dOut['response'] && !$dOut['status']){
-		@mysql_query("UPDATE watchDog set status = 'Case Watch Started', response = '$response', watchStart = NOW(), lastChecked = NOW() where watchID = '$id'");
+		@mysql_query("UPDATE watchDog set status = 'Case Watch Started', response = '$response', watchStart = NOW(), lastChecked = NOW() where watchID = '$id'") or die (mysql_error());
 		error_log(date('r')." Started Watch: Current cases found under ".$dOut['firstName']." ".$dOut['lastName']." in ".$dOut['county'].", ".$response." cases \n", 3, '/logs/watchdog.log');
 		//mail('service@mdwestserve.com','Started Case Watch Dog for '.strtoupper($dOut['firstName']).' '.strtoupper($dOut['lastName']),'Current cases found under '.$dOut['firstName'].' '.$dOut['lastName'].' in '.$dOut['county'].', '.$response.' cases.');
 	}elseif (trim($dOut['response']) < trim($response)){ 
-		@mysql_query("UPDATE watchDog set status = 'New Case Found', lastResult = '$response', lastChecked = NOW() where watchID = '$id'");
+		@mysql_query("UPDATE watchDog set status = 'New Case Found', lastResult = '$response', lastChecked = NOW() where watchID = '$id'") or die (mysql_error());
 		error_log(date('r').' New case found under '.$dOut['firstName'].' '.$dOut['lastName'].' in '.$dOut['county'].', '.$response.' cases (up from '.$dOut['response'].')'."  \n", 3, '/logs/watchdog.log');
 		//mail('service@mdwestserve.com','Case Watch Dog for '.strtoupper($dOut['firstName']).' '.strtoupper($dOut['lastName']),'New case found under '.$dOut['firstName'].' '.$dOut['lastName'].' in '.$dOut['county'].', '.$response.' cases (up from '.$dOut['response'].')');
 	}else{
-		@mysql_query("UPDATE watchDog set status='Searching...', lastChecked = NOW(), lastResult = '$response' where watchID = '$id'");
+		@mysql_query("UPDATE watchDog set status='Searching...', lastChecked = NOW(), lastResult = '$response' where watchID = '$id'") or die (mysql_error());
 	}
 }
-error_log(date('r')." Watchdog Search Complete \n", 3, '/logs/watchdog.log');
-
+//error_log(date('r')." Watchdog Search Complete \n", 3, '/logs/watchdog.log');
+$result=ob_get_clean();
+if (trim($result) != ''){
+	error_log(date('r')." Watchdog Error: $result \n", 3, '/logs/watchdog.log');
+}
 ?>
