28effe137b5a2bb78a55b01aa141408f02d2e56f
zsalwen <zach@mdwestserve.com>
2011-02-07 16:44:40 -0500

    'list'

--- penalize.php
+++ penalize.php
@@ -147,7 +147,7 @@ $def='';
 </form>
 <?
 // If $_GET[list] variable is present, display 
-if ($_GET[list]){
+if ($_GET['list']){
 	$def='';
 	if ($_GET[defendant]){
 		$def = " AND defendantID='$_GET[defendant]'";
