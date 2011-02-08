e2c66c7eeda2a0f164adab81ac2b1907d19b3737
Patrick McGuire <insidenothing@gmail.com>
2011-02-07 16:55:53 -0800

    post and echo

--- settings/attributeManager.php
+++ settings/attributeManager.php
@@ -1,6 +1,61 @@
 <?
 mysql_connect();
 mysql_select_db('service');
-
-
+ini_set('error_reporting',E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors','Off');
+if ($_POST[name] && $_POST[description]){
+@mysql_query("insert into attribute (name,description) values ( '$_POST[name]','$_POST[description]')");
+}
+if ($_GET[undeleteAttribute]){
+  @mysql_query("update attribute set status='active' where id = '$_GET[undeleteAttribute]' ");
+}
+if ($_GET[deleteAttribute]){
+  @mysql_query("update attribute set status='inactive' where id = '$_GET[deleteAttribute]' ");
+}
 ?>
+<style>
+small { color:#FF0000; }
+</style>
+<hr>
+<h3>Current System Layout:</h3>
+Attributes 
+<table border="1">
+ <tr>
+  <td><b>Attribute ID#<b></td>
+  <td>Description</td>
+ </tr>
+<? 
+$r=@mysql_query("select * from attribute where status='active'");
+while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){ ?>
+ <tr>
+  <td><b><?=$d[name];?></b></td>
+  <td><?=$d[description];?></td>
+ </tr>
+<? } ?>
+</table>
+
+
+
+
+
+
+
+
+
+<form method="post">
+This is the form to add new attributes<br>
+<table>
+<tr>
+ <td>Name</td>
+  <td>Description</td>
+</td>
+<tr>
+  <td><input name="name"></td>
+  <td><input name="description"></td>
+</td>
+</table>
+<input type="submit">
+</form>
+
+
+
