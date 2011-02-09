c476cba2c2adad1fb4d099f9be7103f267ef37a0
Patrick McGuire <insidenothing@gmail.com>
2011-02-09 08:01:22 -0800

    only merge packet id to start

--- affidavitMaster/merge.php
+++ affidavitMaster/merge.php
@@ -1,19 +1,31 @@
 <?
 // this is the dual layer merge system
+if ($_GET[packet]){ $packet = $_GET[packet]; }else{ die("Missing packet id '?template='"); }
+if ($_GET[template]){ $template = $_GET[packet]; }else{ die("Missing template id '?template='"); }
+if ($_GET[affidavit]){ $affidavit = $_GET[affidavit]; }else{ die("Missing affidavit id '&affidavit='"); }
 mysql_connect();
 mysql_select_db('service');
-
 // Pull the template
-
-
+$q = "SELECT * FROM template WHERE id = '$template'";
+$r = @mysql_query ($q) or die(mysql_error());
+$d = mysql_fetch_array($r, MYSQL_ASSOC);
+$base = stripslashes($d[html]); // we are done with $d, good to reuse
+if (!$base){ die('Missing template html "$body" '); }
+// Pull the data we need. (this is where we join!)
+$q = "SELECT * FROM packets WHERE id = '$template'";
+$r = @mysql_query ($q) or die(mysql_error());
+$d = mysql_fetch_array($r, MYSQL_ASSOC);
+if (!$d[id]){ die('Missing packet data "$d[id]" '); }
 // merge the data
-$contents = str_replace('[DEPOSIT]', $bid_deposit, $contents);
-$contents = str_replace('[FS/GR]', $gr_desc, $contents);
-$contents = str_replace('[GRPARA]', $gr_para, $contents);
-$contents = str_replace('[GRPARAW]', $gr_paraw, $contents);
+$base = str_replace('[ID]', $d[id], $base);
+
+
+
+
+
+
 
 // Put the final affidavit
-
-
-
+@mysql_query("update affidavits set html =' ".addslashes($base)." ' where id = '$affidavit' ");
+mysql_close();
 ?>
