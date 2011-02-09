ba24f20547c6ee276f01eac73d01e524155633bd
Patrick McGuire <insidenothing@gmail.com>
2011-02-09 07:11:26 -0800

    disable form until action adds an additional attachment instead of replacing it

--- merge.php
+++ merge.php
@@ -3,7 +3,7 @@
 	echo "window.location.href='http://staff.mdwestserve.com/otd/temp/pdfConcat.php?src=".$_POST[src]."&dest=".$_POST[dest]."';";
 }*/
 ?>
-<form action="http://staff.mdwestserve.com/temp/pdfConcat.php">
+<!--<form action="http://staff.mdwestserve.com/temp/pdfConcat.php">-->
 <table align="center">
 	<tr>
 		<td colspan="2" align="center"><h1 style="letter-spacing: 5px;">MERGE PDFs</h1><br><span style="font-size: 12px; font-variant:small caps;">WARNING: THIS PROGRAM <i>WILL</i> REPLACE THE DESTINATION OTD FILE WITH A MERGED PDF.  USE WITH CAUTION.</span></td>
