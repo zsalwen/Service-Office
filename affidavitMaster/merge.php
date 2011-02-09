398a6a6dea4365fe90a5e48bcc67b85577e439e9
Patrick McGuire <insidenothing@gmail.com>
2011-02-09 08:57:56 -0800

    progressing well, into to attribute manger

--- affidavitMaster/merge.php
+++ affidavitMaster/merge.php
@@ -9,20 +9,30 @@ mysql_select_db('service');
 $q = "SELECT * FROM template WHERE id = '$template'";
 $r = @mysql_query ($q) or die(mysql_error());
 $d = mysql_fetch_array($r, MYSQL_ASSOC);
-$base = stripslashes($d[html]); // we are done with $d, good to reuse
+$base = stripslashes($d[html]); 
 if (!$base){ die('Missing template html "$body" '); }
-// Pull the data we need. (this is where we join!)
+
+// Pull the data we need. (when we are done, pick the joins)
 $q = "SELECT * FROM packet WHERE id = '$packet'";
 $r = @mysql_query ($q) or die(mysql_error());
-$d = mysql_fetch_array($r, MYSQL_ASSOC);
-if (!$d[id]){ die('Missing packet data "$d[id]" '); }
+$packet = mysql_fetch_array($r, MYSQL_ASSOC);
+if (!$packet[id]){ die('Missing packet table data "$packet[id]" '); }
+
+/*
+$q = "SELECT * FROM server WHERE id = '' "; // pull server information get id from ????
+$r = @mysql_query ($q) or die(mysql_error());
+$server = mysql_fetch_array($r, MYSQL_ASSOC);
+if (!$server[id]){ die('Missing server table data "$server[id]" '); }
+*/
+
 // merge the data
-$base = str_replace('[ID]', $d[id], $base);
+$base = str_replace('[ID]', $packet[id], $base); 
 
-
-
-
-
+// attribute manager (per table)
+$r=@mysql_query("select * from attribute where table_name = 'server'");
+while($attribute = mysql_fetch_array($r,MYSQL_ASSOC)){
+$base = str_replace($attribute[merge_name]', $server[$attribute[field_name]], $base); //hardcode
+}
 
 
 // Put the final affidavit
