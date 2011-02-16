98257c41b275d6ae7a31b1c80aebf4237a294b45
Patrick McGuire <insidenothing@gmail.com>
2011-02-16 05:27:15 -0800

    use right array!

--- affidavitMaster/merge.php
+++ affidavitMaster/merge.php
@@ -89,8 +89,8 @@ $break = $attribute[advancedResult];
 echo "<li>run resolved query  (".$resolved.") results into ".$attribute[merge_name]." below</li>";
 $rSub=@mysql_query($resolved) or die('<br>Error in Query: '.$resolved.'<br>'.mysql_error());
 while($advanced = mysql_fetch_array($rSub,MYSQL_ASSOC)){
-echo "<li>compiling $rSub[compiled] into $attribute[merge_name]</li>";
-$compiled .= $rSub[compiled].$break;
+echo "<li>compiling $advanced[compiled] into $attribute[merge_name]</li>";
+$compiled .= $advanced[compiled].$break;
 }
 $base = str_replace($attribute[merge_name], $compiled, $base); 
 }
