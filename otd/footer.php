659575aeb9e268edae5e9e57b32b0d710aa5161f
Patrick McGuire <insidenothing@gmail.com>
2011-02-23 11:29:58 -0800

    show mirror and lb

--- otd/footer.php
+++ otd/footer.php
@@ -1,9 +1,10 @@
 <?
 mysql_close();
 $headers = apache_request_headers(); 
-$mirror = $headers["X-Forwarded-Host"];
+$lb = $headers["X-Forwarded-Host"];
+$mirror = $_SERVER['HTTP_HOST'];
 ?>
-<center style="padding:0px;">Mysql Closed on <?=$mirror;?></center>
+<center style="padding:0px;">Mysql Closed on <?=$mirror;?> from <?=$lb;?></center>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
