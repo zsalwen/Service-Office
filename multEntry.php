8232363b47d4f3e608e5401d2aa783f9b4788f84
zsalwen <zach@mdwestserve.com>
2011-03-04 12:17:24 -0500

    Removing while loop

--- multEntry.php
+++ multEntry.php
@@ -139,27 +139,17 @@ if ($_GET[otd] != ''){
 	$otd=explode("|",$_GET[otd]);
 }
 foreach( $otd as $key => $value){
-	if ($value != ''){
+	//if ($value != ''){
 		echo "<li>[OTD$value]</li>";
 		//@mysql_query("update ps_packets set courierID = '$_POST[courier]', estFileDate='$_POST[newEst]' where packet_id = '$key'");
-	}
+	//}
 }
 echo "<hr>";
 foreach( $ev as $key => $value){
-	if ($value != ''){
+	//if ($value != ''){
 		echo "<li>[EV$value]</li>";
 		//@mysql_query("update evictionPackets set courierID = '$_POST[courier]', estFileDate='$_POST[newEst]' where eviction_id = '$key'");
-	}
-} 
-echo "<hr>";
-$i=-1;
-while($i < count($otd)){$i++;
-	echo "<li>OTD".$otd[$i]."</li>";
-}
-echo "<hr>";
-$i=-1;
-while($i < count($ev)){$i++;
-	echo "<li>EV".$ev[$i]."</li>";
+	//}
 }
 echo "</div>";
 ?>
