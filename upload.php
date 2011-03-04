782b725c95fb8c48285efce5622edf6d603ac9b0
Patrick McGuire <insidenothing@gmail.com>
2011-03-04 00:26:29 -0800

    hrmm

--- upload.php
+++ upload.php
@@ -1,9 +1,8 @@
-
 <script>function MultiSelector( list_target, max ){this.list_target = list_target;this.count = 0;this.id = 0;if( max ){this.max = max;} else {this.max = -1;};this.addElement = function( element ){if( element.tagName == 'INPUT' && element.type == 'file' ){element.name = 'file_' + this.id++;element.multi_selector = this;element.onchange = function(){var new_element = document.createElement( 'input' );new_element.type = 'file';this.parentNode.insertBefore( new_element, this );this.multi_selector.addElement( new_element );this.multi_selector.addListRow( this );this.style.position = 'absolute';this.style.left = '-1000px';};if( this.max != -1 && this.count >= this.max ){element.disabled = true;};this.count++;this.current_element = element;} else {alert( 'Error: not a file input element' );};};this.addListRow = function( element ){var new_row = document.createElement( 'div' );var new_row_button = document.createElement( 'input' );new_row_button.type = 'button';new_row_button.value = 'Delete';new_row.element = element;new_row_button.onclick= function(){this.parentNode.element.parentNode.removeChild( this.parentNode.element );this.parentNode.parentNode.removeChild( this.parentNode );this.parentNode.element.multi_selector.count--;this.parentNode.element.multi_selector.current_element.disabled = false;return false;};new_row.innerHTML = element.value;new_row.appendChild( new_row_button );this.list_target.appendChild( new_row );};};<script>    
 
 
 
-<form enctype="multipart/form-data" action="your_script_here.script" method = "post">
+<form enctype="multipart/form-data" action="upload.php" method = "post">
 	<!-- The file element -- NOTE: it has an ID -->
 	<input id="my_file_element" type="file" name="file_1" >
 	<input type="submit">
@@ -13,7 +12,7 @@ Files:
 <div id="files_list"></div>
 <script>
 	<!-- Create an instance of the multiSelector class, pass it the output target and the max number of files -->
-	var multi_selector = new MultiSelector( document.getElementById( 'files_list' ), 3 );
+	var multi_selector = new MultiSelector( document.getElementById( 'files_list' ), 20 );
 	<!-- Pass in the file element -->
 	multi_selector.addElement( document.getElementById( 'my_file_element' ) );
 </script>
