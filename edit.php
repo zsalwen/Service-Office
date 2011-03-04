db9a0499ee04d60c5149a241b4d80a7b97256a12
Patrick McGuire <insidenothing@gmail.com>
2011-03-03 22:56:55 -0800

    get working set

--- edit.php
+++ edit.php
@@ -329,7 +329,7 @@ $dc=mysql_fetch_array($rc,MYSQL_ASSOC);
 <LEGEND ACCESSKEY=C>Service Links</LEGEND>
 <table style="padding:0px; font-weight:bold; border-collapse:collapse; height:150px !important; font-size:11px;" cellpadding="0" cellspacing="0">
 	<tr>
-		<td><a href="http://staff.mdwestserve.com/instruction.php?packet=<?=$d[packet_id]?>" target="preview">Add instruction set</a></td>
+		<td><a href="http://staff.mdwestserve.com/instruction.php?packet=<?=$packet?>" target="preview">Add instruction set</a></td>
 	</tr>
 	<tr>
 		<td><a href="http://staff.mdwestserve.com/otd/minips_pay.php?id=<?=$d[packet_id]?>" target="preview">Payments</a></td>
