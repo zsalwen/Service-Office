13689e19a7e9c6c8a3b84a289ad40bf3c96e1c94
Patrick McGuire <insidenothing@gmail.com>
2011-03-07 07:25:10 -0800

    basic setup

--- details.php
+++ details.php
@@ -1,3 +1,10 @@
-<?
-// container above edit
-?>
+<table style="width:100%; height:100%; background-color:#999999; color:#FFFF99">
+<tr>
+<td colspan="3" style="height:50px;" align="center">AJAX Information Bar</td>
+</tr>
+<tr>
+<td><iframe name="pane1" id="pane1" style="width:100%; height:100%;"></iframe></td>
+<td valign="center">Include Menu</td>
+<td><iframe name="pane2" id="pane2" style="width:100%; height:100%;"></iframe></td>
+</tr>
+</table>
