7c8f2896efd8548b929f49e806e8c51deb3368b2
Patrick McGuire <insidenothing@gmail.com>
2011-03-07 08:31:04 -0800

    center width=100px

--- details.php
+++ details.php
@@ -9,7 +9,7 @@
 </tr>
 <tr>
 <td><iframe name="pane1" id="pane1" style="width:100%; height:100%;"></iframe></td>
-<td valign="center"><? include 'details.menu.php';?></td>
+<td valign="center" style="width:100px;"><? include 'details.menu.php';?></td>
 <td><iframe name="pane2" id="pane2" style="width:100%; height:100%;"></iframe></td>
 </tr>
 </table>
