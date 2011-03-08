052ec5000897103704f546a981e13b974a501743
Patrick McGuire <insidenothing@gmail.com>
2011-03-08 11:02:29 -0800

    basic interface

--- dropbox.php
+++ dropbox.php
@@ -1,5 +1,45 @@
 <?
-// this file will handle attachments on a per packet subroutine
-
+if (!$_COOKIE[psdata][user_id]){
+error_log(date('h:iA j/n/y')." SECURITY PREVENTED ACCESS to ".$_SERVER['SCRIPT_NAME']." by ".$_SERVER["REMOTE_ADDR"]."\n", 3, '/logs/user.log');
+header ('Location: http://staff.mdwestserve.com');
+}
+mysql_connect();
+mysql_select_db('core');
 ?>
-dropbox.php
+</head>
+<body>
+<fieldset>
+<legend>Attachments</legend>
+<table border="1">
+<tr>
+<td>Date Received</td>
+<td></td>
+<td></td>
+<td>Description</td>
+<td>Status</td>
+</tr>
+<?
+$r=@mysql_query("select * from attachment where packet_id = '$_GET[packet]'");
+while($d=mysql_fetch_array($r,MYSQL_ASSOC)){
+?>
+ <tr>
+<td><?=$d[processed];?></td>
+<td>
+<img onClick="parent.frames['pane1'].location.href = 'attachment.php?id=<?=$d[id];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgLeftArrow.gif" border="0"> 
+Open 
+<img onClick="parent.frames['pane2'].location.href = 'attachment.php?id=<?=$d[id];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgRightArrow.gif" border="0">
+</td>
+<td>
+<img onClick="parent.frames['pane1'].location.href = '<?=$d[absolute_url];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgLeftArrow.gif" border="0"> 
+Open 
+<img onClick="parent.frames['pane2'].location.href = '<?=$d[absolute_url];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgRightArrow.gif" border="0">
+</td>
+<td><?=$d[description];?></td>
+</tr>
+<? }?>
+</table>
+}
+?>
+</fieldset>
+</body>
+</html>
