77402d258e41c3afb83457eaa5d301ff24e05f35
Patrick McGuire <insidenothing@gmail.com>
2011-03-08 11:04:14 -0800

    show status

--- dropbox.php
+++ dropbox.php
@@ -3,6 +3,10 @@ if (!$_COOKIE[psdata][user_id]){
 error_log(date('h:iA j/n/y')." SECURITY PREVENTED ACCESS to ".$_SERVER['SCRIPT_NAME']." by ".$_SERVER["REMOTE_ADDR"]."\n", 3, '/logs/user.log');
 header ('Location: http://staff.mdwestserve.com');
 }
+?>
+<html>
+<head>
+<?
 mysql_connect();
 mysql_select_db('core');
 ?>
@@ -26,7 +30,7 @@ while($d=mysql_fetch_array($r,MYSQL_ASSOC)){
 <td><?=$d[processed];?></td>
 <td>
 <img onClick="parent.frames['pane1'].location.href = 'attachment.php?id=<?=$d[id];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgLeftArrow.gif" border="0"> 
-Open 
+Edit 
 <img onClick="parent.frames['pane2'].location.href = 'attachment.php?id=<?=$d[id];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgRightArrow.gif" border="0">
 </td>
 <td>
@@ -35,11 +39,10 @@ Open
 <img onClick="parent.frames['pane2'].location.href = '<?=$d[absolute_url];?>'; " src="http://connect.stern.nyu.edu/zimbra/img/startup/ImgRightArrow.gif" border="0">
 </td>
 <td><?=$d[description];?></td>
+<td><?=$d[status];?></td>
 </tr>
 <? }?>
 </table>
-}
-?>
 </fieldset>
 </body>
 </html>
