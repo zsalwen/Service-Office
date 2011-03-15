9cd60f447d822f99366e532e2902968912825acb
zsalwen <zach@mdwestserve.com>
2011-03-15 10:30:04 -0400

    width, centering

--- standard/serviceSheet.php
+++ standard/serviceSheet.php
@@ -255,7 +255,7 @@ function serviceSheet($packet){
 	<? } 
 	}
 	if (trim($d[accountingNotes]) != ''){
-		echo "<table align='center' border='1' style='border-collapse:collapse;'><tr><td><b>ACCOUNTING NOTES</b><br>".strtoupper(stripslashes($d[accountingNotes]))."</td></tr></table>";
+		echo "<table width='100%' align='center' border='1' style='border-collapse:collapse;'><tr><td align='center'><b>ACCOUNTING NOTES</b><br>".strtoupper(stripslashes($d[accountingNotes]))."</td></tr></table>";
 	}
 	?>
 	</td></tr></table>
