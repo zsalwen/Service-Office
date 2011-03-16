573ab694cd6be3806a203baee4e27177601f59d5
Patrick McGuire <insidenothing@gmail.com>
2011-03-16 08:17:17 -0700

    show sample time

--- network.php
+++ network.php
@@ -59,6 +59,7 @@ $results3b = my_ssh('ww2.mdwestserve.com','grep ServerName /etc/httpd/conf/httpd
 $results5b = my_ssh('ww2.mdwestserve.com','ssh 10.0.0.3 grep ServerName /etc/httpd/conf/httpd.conf');
 ?>
 <meta http-equiv="refresh" content="300" />
+<div style="font-size:25px;"><?=date('r');?></div>
 <table border="1">
 <tr>
 <td>Host</td>
