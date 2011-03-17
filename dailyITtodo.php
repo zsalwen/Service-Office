3488fb8f903bfd846e068bb115ad74298c435425
Patrick McGuire <insidenothing@gmail.com>
2011-03-17 11:09:07 -0700

    style

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -5,13 +5,14 @@ mysql_select_db('apache');
 if($_GET[done]){ @mysql_query("delete from apacheErrors where id = '$_GET[done]' "); }
 $r=@mysql_query("select id, counter, lastTime, message from apacheErrors order by counter DESC, lastTime DESC");
 ?>
-<table>
+<h1>The first job in IT is error detection and correction.</h1>
+<h3><?=date('r');?></h3>
+<table border="1">
 <? while($d=mysql_fetch_array($r,MYSQL_ASSOC)){ ?>
 <tr>
 <td><?=$d[counter];?></td>
 <td><?=$d[lastTime]?></td>
-<td><?=$d[message]?></td>
-<td><a href="?done=<?=$d[id]?>">mark fixed</a></td>
+<td><?=$d[message]?>,<a href="?done=<?=$d[id]?>">mark fixed</a></td>
 </tr>
 <? } ?>
 </table>
