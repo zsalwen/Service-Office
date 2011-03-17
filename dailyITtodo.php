4d90c0ce3eacd8ba67dd4765e2ab80917bccfd15
Patrick McGuire <insidenothing@gmail.com>
2011-03-17 10:49:38 -0700

    function to remove items

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -2,7 +2,8 @@
 include 'lock.php';
 mysql_connect();
 mysql_select_db('apache');
-$r=@mysql_query("select counter, lastTime, message from apacheErrors order by counter DESC, lastTime DESC");
+if($_GET[done]){ @mysql_query("delete from apacheErrors where id = '$_GET[done]' "); |
+$r=@mysql_query("select id, counter, lastTime, message from apacheErrors order by counter DESC, lastTime DESC");
 ?>
 <table>
 <? while($d=mysql_fetch_array($r,MYSQL_ASSOC)){ ?>
@@ -10,6 +11,7 @@ $r=@mysql_query("select counter, lastTime, message from apacheErrors order by co
 <td><?=$d[counter];?></td>
 <td><?=$d[lastTime]?></td>
 <td><?=$d[message]?></td>
+<td><a href="?done=<?=$d[id]?>">mark fixed</a></td>
 </tr>
 <? } ?>
 </table>
