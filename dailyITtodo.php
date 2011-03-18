b613b2020729ca8a1c9ea94c0f37281c5cd16607
Patrick McGuire <insidenothing@gmail.com>
2011-03-18 09:34:45 -0700

    pull status

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -6,7 +6,7 @@ if($_GET[done]){ @mysql_query("delete from apacheErrors where id = '$_GET[done]'
 if($_GET[zach]){ @mysql_query("update apacheErrors set status = 'Assigned to Zach'  where id = '$_GET[zach]' "); }
 if($_GET[runner]){ @mysql_query("update apacheErrors set status = 'Assigned to Runner'  where id = '$_GET[runner]' "); }
 if($_GET[patrick]){ @mysql_query("update apacheErrors set status = 'Assigned to Patrick'  where id = '$_GET[patrick]' "); }
-$r=@mysql_query("select id, counter, lastTime, message,server from apacheErrors order by counter DESC, lastTime DESC");
+$r=@mysql_query("select id, counter, lastTime, message,server,status from apacheErrors order by counter DESC, lastTime DESC");
 ?>
 <meta http-equiv="refresh" content="60;url=http://staff.mdwestserve.com/dailyITtodo.php"> 
 <h1>The first job in IT is error detection and correction.</h1>
