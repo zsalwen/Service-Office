9e8eb219c4ae681eb7ccabb138b105e9e6332c32
Patrick McGuire <insidenothing@gmail.com>
2011-03-18 09:01:09 -0700

    select server for display

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -5,7 +5,7 @@ mysql_select_db('apache');
 if($_GET[done]){ @mysql_query("delete from apacheErrors where id = '$_GET[done]' "); }
 if($_GET[all]){ @mysql_query("delete from apacheErrors"); }
 if($_GET[status]){ @mysql_query("update apacheErrors set status = '$_GET[status]'  where id = '$_GET[done]' "); }
-$r=@mysql_query("select id, counter, lastTime, message from apacheErrors order by counter DESC, lastTime DESC");
+$r=@mysql_query("select id, counter, lastTime, message,server from apacheErrors order by counter DESC, lastTime DESC");
 ?>
 <meta http-equiv="refresh" content="60;url=http://staff.mdwestserve.com/dailyITtodo.php"> 
 <h1>The first job in IT is error detection and correction.</h1>
