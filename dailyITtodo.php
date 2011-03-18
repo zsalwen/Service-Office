7d215bd048f07be8588e8dbd5646e899dfe12183
Patrick McGuire <insidenothing@gmail.com>
2011-03-18 09:32:28 -0700

    assign status

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -3,21 +3,22 @@ include 'lock.php';
 mysql_connect();
 mysql_select_db('apache');
 if($_GET[done]){ @mysql_query("delete from apacheErrors where id = '$_GET[done]' "); }
-if($_GET[all]){ @mysql_query("delete from apacheErrors"); }
-if($_GET[status]){ @mysql_query("update apacheErrors set status = '$_GET[status]'  where id = '$_GET[done]' "); }
+if($_GET[zach]){ @mysql_query("update apacheErrors set status = 'Assigned to Zach'  where id = '$_GET[zach]' "); }
+if($_GET[runner]){ @mysql_query("update apacheErrors set status = 'Assigned to Runner'  where id = '$_GET[runner]' "); }
+if($_GET[patrick]){ @mysql_query("update apacheErrors set status = 'Assigned to Patrick'  where id = '$_GET[patrick]' "); }
 $r=@mysql_query("select id, counter, lastTime, message,server from apacheErrors order by counter DESC, lastTime DESC");
 ?>
 <meta http-equiv="refresh" content="60;url=http://staff.mdwestserve.com/dailyITtodo.php"> 
 <h1>The first job in IT is error detection and correction.</h1>
 <h3><?=date('r');?></h3>
- <a href="?all=1">clear all</a>
 <table border="1">
 <? while($d=mysql_fetch_array($r,MYSQL_ASSOC)){ ?>
 <tr>
 <td><?=$d[counter];?></td>
 <td><?=$d[server]?></td>
+<td><?=$d[status]?></td>
 <td><?=$d[lastTime]?></td>
-<td><?=$d[message]?>, <a href="?done=<?=$d[id]?>">clear</a></td>
+<td><?=$d[message]?>, <a href="?done=<?=$d[id]?>">clear</a>, <a href="?zach=<?=$d[id]?>">Zach</a>, <a href="?runner=<?=$d[id]?>">Runner</a>, <a href="?patrick=<?=$d[id]?>">Patrick</a> </td>
 </tr>
 <? } ?>
 </table>
