4b63ef4cd281c857f309fa33a7c0b3d8715349f1
Patrick McGuire <insidenothing@gmail.com>
2011-03-22 14:02:37 -0700

    switch to product files received

--- graph/OTDreceivedGraph.php
+++ graph/OTDreceivedGraph.php
@@ -23,6 +23,7 @@ while ($year <= $curYear){
 		}else{
 			$i2=$i;
 		}
+// the is the all files line
 		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%'");
 		$received["$i"]=mysql_num_rows($r);
 		if ($received["$i"] > 0){}else{
@@ -39,8 +40,8 @@ while ($year <= $curYear){
 			$src .= ','.$received["$i"];
 		}
 		$src2 .= '|'.monthConvert($i2)." $yr";
-		//pull BURSON files
-		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND attorneys_id='1'");
+		//this is the presale line (was BURSON files)
+		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND product_id='1'");
 		$value1=mysql_num_rows($r);
 		if ($value1 > 0){}else{
 			$value1='0';
@@ -50,8 +51,8 @@ while ($year <= $curYear){
 		}else{
 			$burson .= ','.$value1;
 		}
-		//pull WHITE files
-		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND attorneys_id='3'");
+		//this is the eviction line (was WHITE files)
+		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND product_id='2'");
 		$value2=mysql_num_rows($r);
 		if ($value2 > 0){}else{
 			$value2='0';
@@ -61,8 +62,8 @@ while ($year <= $curYear){
 		}else{
 			$white .= ','.$value2;
 		}
-		//pull BGW files
-		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND attorneys_id='70'");
+		//this is the standard line (was BGW files)
+		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND product_id='3'");
 		$value3=mysql_num_rows($r);
 		if ($value3 > 0){}else{
 			$value3='0';
@@ -72,8 +73,8 @@ while ($year <= $curYear){
 		}else{
 			$bgw .= ','.$value3;
 		}
-		//pull OTHER files
-		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND attorneys_id <> '1' AND attorneys_id <> '3' AND attorneys_id <> '70'");
+		//this is the mail only line (was OTHER files)
+		$r=mysql_query("SELECT id FROM packet WHERE date_received LIKE '%$year-$i2%' AND product_id = '4'");
 		$value4=mysql_num_rows($r);
 		if ($value4 > 0){}else{
 			$value4='0';
@@ -96,10 +97,10 @@ $z4=number_format($z1*4,0);
 /*echo "<table><tr>";
 echo "<td></td>".str_replace('|','</td><td>',$src2).'</td></tr>';
 echo "<td>ALL:</td><td>".str_replace(',','</td><td>',$src).'</td></tr>';
-echo "<tr><td>BURSON:</td><td>".str_replace(',','</td><td>',$burson).'</td></tr>';
-echo "<tr><td>WHITE:</td><td>".str_replace(',','</td><td>',$white).'</td></tr>';
-echo "<tr><td>BGW:</td><td>".str_replace(',','</td><td>',$bgw).'</td></tr>';
-echo "<tr><td>OTHER:</td><td>".str_replace(',','</td><td>',$other).'</td>';
+echo "<tr><td>PRESALE:</td><td>".str_replace(',','</td><td>',$burson).'</td></tr>';
+echo "<tr><td>EVICTION:</td><td>".str_replace(',','</td><td>',$white).'</td></tr>';
+echo "<tr><td>STANDARD:</td><td>".str_replace(',','</td><td>',$bgw).'</td></tr>';
+echo "<tr><td>MAILONLY:</td><td>".str_replace(',','</td><td>',$other).'</td>';
 echo "</tr></table>";*/
 $src="http://1.chart.apis.google.com/chart?cht=lc&chs=1000x300&chd=t:".$src."|".$burson."|".$white."|".$bgw."|".$other."&chxl=0:".$src2."|1:|0|$z1|$z2|$z3|$z4|$z&chtt=All Service Files Received 2008-$curYear&chdl=All Files|Burson|White|BGW|Others&chco=FF0000,00FF00,0000FF,800080,FF8040&chls=1,1,0|1,1,0|1,1,0|1,1,0|1,1,0";
 $rest="&chxt=x,y&chds=0,".$z."&chxtc=0,10|1,-980&chxs=0,000000,7|1,000000,10,-1,lt,333333&chm=t$z$zzz,000000,0,$zz,10";
