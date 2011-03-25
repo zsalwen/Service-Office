7087406c73d4e965df8637da3f57d21e5205eb27
Patrick McGuire <insidenothing@gmail.com>
2011-03-24 18:54:06 -0700

    ok

--- administrator/index.php
+++ administrator/index.php
@@ -1,12 +1,12 @@
 <?
 if (!$_COOKIE[admin][name]){
-header('Location: http://staff.mdwestserve.com/adminstrator/login.php');
+header('Location: http://staff.mdwestserve.com/administrator/login.php');
 }
 ?>
 <table width="100%"><tr>
 foreach (glob("modules/*.php") as $filename)
 {
-     echo "<td><a href='$filename' target='box'>".strtoupper(str_replace('.php','',$filename))."</a></td>";
+     echo "<td><a href='modules/$filename' target='box'>".strtoupper(str_replace('.php','',$filename))."</a></td>";
 }
 ?>
 </tr></table>
