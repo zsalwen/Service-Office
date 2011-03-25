b4efcd01f2f8bc1d8f58a5df1b355fb257db467f
Patrick McGuire <insidenothing@gmail.com>
2011-03-24 18:57:03 -0700

    open php up

--- administrator/index.php
+++ administrator/index.php
@@ -4,6 +4,7 @@ header('Location: http://staff.mdwestserve.com/administrator/login.php');
 }
 ?>
 <table width="100%"><tr>
+<?
 foreach (glob("modules/*.php") as $filename)
 {
      echo "<td><a href='modules/$filename' target='box'>".strtoupper(str_replace('.php','',$filename))."</a></td>";
