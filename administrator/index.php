15b89a175a9b6efaddb4530a7b15e1ab9154b22d
Patrick McGuire <insidenothing@gmail.com>
2011-03-24 18:46:41 -0700

    frame base system

--- administrator/index.php
+++ administrator/index.php
@@ -1,6 +1,13 @@
 <?
-foreach (glob("modules/*.php") as $filename)
-{
-    include $filename;
+if (!$_COOKIE[admin][name]){
+header('Location: http://staff.mdwestserve.com/adminstrator/login.php');
 }
 ?>
+<table width="100%"><tr>
+foreach (glob("modules/*.php") as $filename)
+{
+     echo "<td><a href='$filename' target='box'>".strtoupper(str_replace('.php','',$filename))."</a></td>";
+}
+?>
+</tr></table>
+<center><iframe name="" id ="" style="width:99%; height:99%;"></iframe></center>
