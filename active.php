c2a46f418cfba12486d65e80eb2eb4bf763bd50a
zsalwen <zach@mdwestserve.com>
2011-04-04 11:00:50 -0400

    Displaying standard_packets

--- active.php
+++ active.php
@@ -215,6 +215,44 @@ $data.='</ol>';
 return $data;
 }
 
+function standardActiveList($id,$letter){ $_SESSION[active]++;
+	$data='<ol>';
+	$r=@mysql_query("select packet_id, avoidDOT, reopenDate, date_received, filing_status, request_close, request_closea, request_closeb, request_closec, request_closed, request_closee, affidavit_status, service_status, circuit_court, dispatchDate, attorneys_id, estFileDate, rush, TIMEDIFF( NOW(), date_received) as hours, DATEDIFF( CURDATE(), reopenDate) as reopenHours, DATEDIFF(estFileDate, CURDATE()) as estHours from standard_packets where server_id$letter='$id' and (process_status = 'Assigned' OR process_status = 'ASSIGNED') order by  packet_id");
+	while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){ $_SESSION[active2]++;
+		$_SESSION[active3]++;
+		$estHours=($d[estHours]*24)-date('G');
+		if ($d[filing_status] == 'REOPENED'){
+			$hours=$d[reopenHours]*24;
+			$reopenDate=explode('-',$d[reopenDate]);
+			$reopenDate=$reopenDate[1].'-'.$reopenDate[2];
+			$reopen = " <span style='background-color:#FFFFFF; color:000000 !important;'><small>ReO:&nbsp;$reopenDate</small></span>";
+		}else{
+			$hours=stripHours($d[hours]);
+			$reopen='';
+		}
+		$reopen .= " <span style='background-color:#AAAAAA; color:FFFFFF;'>DISP:&nbsp;".justDate2($d[dispatchDate])."</span>";
+		if ($d[avoidDOT] != ''){
+			$reopen .= " <span style='background-color:#000000; color:FF0000; border: 3px solid red; font-weight:bold;'>AvoidDOT</span>";
+		}
+		if ($d)
+		$estFileDate=explode('-',$d[estFileDate]);
+		$estFileDate=$estFileDate[1].'-'.$estFileDate[2];
+		$reopen .= " <span title='$estHours Hours Remaining' style='background-color:".colorCode2($estHours)." border: 1px solid black;'>FILE:&nbsp;".$estFileDate."</span>";
+		if ($d[rush] != ''){
+			$reopen .= " <span style='background-color:#000000; color:FF00FF; border: 3px solid black; font-weight:bold;'>RUSH</span>";
+		}
+		if ($hours > $_SESSION[cap]){
+			$data .= "<li title='Affidavit: $d[affidavit_status] Service Status: $d[service_status]' style='background-color:".colorCode($hours,$d[packet_id],$letter).";'>";
+			if ($d[request_close] || $d[request_closea] || $d[request_closeb] || $d[request_closec] || $d[request_closed] || $d[request_closee]){
+				$data .= "<a href='http://service.mdwestserve.com/wizard.php?jump=".$d[packet_id]."-1' target='_blank' style='background-color:#00FFFF;'><b>QC</b></a> ";
+			}
+			$data .= "<a href='http://staff.mdwestserve.com/standard/order.php?packet=$d[packet_id]' target='_Blank'>S$d[packet_id]</a>: <strong>".$hours."</strong> ".abbrCounty(strtoupper($d[circuit_court]))." <em> <small>[".id2attorney($d[attorneys_id])."]</small></em> ".$reopen."</li>";
+		}
+	}
+	$data.='</ol>';
+	return $data;
+}
+
 function getServers(){
 	$i=0;
 	$r=@mysql_query("SELECT DISTINCT server_id from ps_packets WHERE process_status = 'ASSIGNED'");
@@ -226,6 +264,28 @@ function getServers(){
 	$r=@mysql_query("SELECT DISTINCT server_id from evictionPackets WHERE process_status = 'ASSIGNED'$exclude");
 	while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
 		$list["$i"] = $d[server_id];
+		$exclude .= " AND server_id <> '$d[server_id]'";
+		$i++;
+	}
+	$r=@mysql_query("SELECT DISTINCT server_id from standard_packets WHERE process_status = 'ASSIGNED'$exclude");
+	while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
+		$list["$i"] = $d[server_id];
+		$i++;
+	}
+	ksort($list);
+	return $list;
+}
+function getServers2($letter){
+	$i=0;
+	$r=@mysql_query("SELECT DISTINCT server_id$letter from ps_packets WHERE process_status = 'ASSIGNED'");
+	while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
+		$list["$i"] = $d["server_id$letter"];
+		$exclude .= " AND server_id <> '".$d["server_id$letter"]."'";
+		$i++;
+	}
+	$r=@mysql_query("SELECT DISTINCT server_id$letter from standard_packets WHERE process_status = 'ASSIGNED'$exclude");
+	while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
+		$list["$i"] = $d["server_id$letter"];
 		$i++;
 	}
 	ksort($list);
@@ -264,6 +324,26 @@ while ($d67=mysql_fetch_array($r67,MYSQL_ASSOC)){
 	$list .="</a></li>";
 }
 
+$r66=@mysql_query("select packet_id, server_id, server_ida, server_idb, server_idc, server_idd, server_ide, affidavit_status2, filing_status, rush, TIMEDIFF( NOW(), date_received) as hours, DATEDIFF( CURDATE(), reopenDate) as reopenHours from standard_packets where affidavit_status2 <> '' order by packet_id"); 
+while ($d66=mysql_fetch_array($r66,MYSQL_ASSOC)){
+	if ($d66[affidavit_status2] == 'REOPENED'){
+		$hours=$d66[reopenHours]*24;
+	}else{
+		$hours=stripHours($d66[hours]);
+	}
+	if ($d66[rush] != ''){
+		$reopen .= "-<span style='background-color:#000000; color:FF00FF; border: 3px solid black; font-weight:bold;'>RUSH</span>";
+	}
+	$list .= "<li><a style='background-color:".colorCode($hours,$d66[packet_id],'')."; font-weight:normal !important;' href='http://staff.mdwestserve.com/standard/order.php?packet=$d66[packet_id]' target='_Blank'>S$d66[packet_id]: $d66[affidavit_status2]";
+	if ($d66[server_id]){ $list .= '('.id2server($d66[server_id]).')';}
+	if ($d66[server_ida]){ $list .= '('.id2server($d66[server_ida]).')';}
+	if ($d66[server_idb]){ $list .= '('.id2server($d66[server_idb]).')';}
+	if ($d66[server_idc]){ $list .= '('.id2server($d66[server_idc]).')';}
+	if ($d66[server_idd]){ $list .= '('.id2server($d66[server_idd]).')';}
+	if ($d66[server_ide]){ $list .= '('.id2server($d66[server_ide]).')';}
+	$list .= "</a>$reopen</li>";
+}
+
 if ($list != ''){
 	echo "<fieldset style='background-color:FF0000;width: 600px;'><legend>Custom Watch List</legend>$list</fieldset></td><td align='right'>";
 }?>
@@ -274,7 +354,7 @@ if ($list != ''){
 $servers=getServers();
 $i=0;
 while ($i < count($servers)){
-	echo "<fieldset><legend>Slot 1: ".id2name($servers["$i"])." #".$servers["$i"]."</legend>".serverActiveList($servers["$i"],'').evictionActiveList($servers["$i"])."</fieldset>";
+	echo "<fieldset><legend>Slot 1: ".id2name($servers["$i"])." #".$servers["$i"]."</legend>".serverActiveList($servers["$i"],'').evictionActiveList($servers["$i"]).standardActiveList($servers["$i"],'')."</fieldset>";
 	$i++;
 }
 /*$q="SELECT DISTINCT server_id from ps_packets WHERE process_status = 'ASSIGNED'";
@@ -283,34 +363,34 @@ while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
 echo "<fieldset><legend>Slot 1: ".id2name($d[server_id])." #$d[server_id]</legend>".serverActiveList($d[server_id],'')."</fieldset>";
 }*/
 ?></td><td valign='top'><?
-$q="SELECT DISTINCT server_ida from ps_packets where process_status = 'ASSIGNED' and server_ida <> ''";
-$r=@mysql_query($q);
-while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-echo "<fieldset><legend>Slot 2: ".id2name($d[server_ida])." #$d[server_ida]</legend>".serverActiveList($d[server_ida],'a')."</fieldset>";
+$serversa=getServers2('a');
+$i=0;
+while ($i < count($serversa)){
+echo "<fieldset><legend>Slot 2: ".id2name($serversa["$i"])." #".$serversa["$i"]."</legend>".serverActiveList($serversa["$i"],'a').standardActiveList($serversa["$i"],'a')."</fieldset>";
 }
 ?></td><td valign='top'><?
-$q="SELECT DISTINCT server_idb from ps_packets where process_status = 'ASSIGNED' and server_idb <> ''";
-$r=@mysql_query($q);
-while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-echo "<fieldset><legend>Slot 3: ".id2name($d[server_idb])." #$d[server_idb]</legend>".serverActiveList($d[server_idb],'b')."</fieldset>";
+$serversb=getServers2('b');
+$i=0;
+while ($i < count($serversb)){
+echo "<fieldset><legend>Slot 3: ".id2name($serversb["$i"])." #".$serversb["$i"]."</legend>".serverActiveList($serversb["$i"],'b').standardActiveList($serversb["$i"],'b')."</fieldset>";
 }
 ?></td><td valign='top'><?
-$q="SELECT DISTINCT server_idc from ps_packets where process_status = 'ASSIGNED' and server_idc <> ''";
-$r=@mysql_query($q);
-while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-echo "<fieldset><legend>Slot 4: ".id2name($d[server_idc])." #$d[server_idc]</legend>".serverActiveList($d[server_idc],'c')."</fieldset>";
+$serversc=getServers2('c');
+$i=0;
+while ($i < count($serversc)){
+echo "<fieldset><legend>Slot 4: ".id2name($serversc["$i"])." #".$serversc["$i"]."</legend>".serverActiveList($serversc["$i"],'c').standardActiveList($serversc["$i"],'c')."</fieldset>";
 }
 ?></td><td valign='top'><?
-$q="SELECT DISTINCT server_idd from ps_packets where process_status = 'ASSIGNED' and server_idd <> ''";
-$r=@mysql_query($q);
-while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-echo "<fieldset><legend>Slot 5: ".id2name($d[server_idd])." #$d[server_idd]</legend>".serverActiveList($d[server_idd],'d')."</fieldset>";
+$serversd=getServers2('d');
+$i=0;
+while ($i < count($serversd)){
+echo "<fieldset><legend>Slot 5: ".id2name($serversd["$i"])." #".$serversd["$i"]."</legend>".serverActiveList($serversd["$i"],'d').standardActiveList($serversd["$i"],'d')."</fieldset>";
 }
 ?></td><td valign='top'><?
-$q="SELECT DISTINCT server_ide from ps_packets where process_status = 'ASSIGNED' and server_ide <> ''";
-$r=@mysql_query($q);
-while ($d=mysql_fetch_array($r,MYSQL_ASSOC)){
-echo "<fieldset><legend>Slot 6: ".id2name($d[server_ide])." #$d[server_ide]</legend>".serverActiveList($d[server_ide],'e')."</fieldset>";
+$serverse=getServers2('e');
+$i=0;
+while ($i < count($serverse)){
+echo "<fieldset><legend>Slot 6: ".id2name($serverse["$i"])." #".$serverse["$i"]."</legend>".serverActiveList($serverse["$i"],'e').standardActiveList($serverse["$i"],'e')."</fieldset>";
 }
 ?></td></tr></table>
 <style>
