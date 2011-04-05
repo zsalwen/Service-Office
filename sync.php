b913759a0cf6d74bf98facf83577cc739ece6dd1
Patrick McGuire <insidenothing@gmail.com>
2011-04-05 08:41:29 -0700

    quick and easy

--- sync.php
+++ sync.php
@@ -1,4 +1,14 @@
 <?
+mysql_connect();
+mysql_select_db('core');
 
+@mysql_query("update sync set status='automated rescan requested' where to_id = '$_GET[packet]' ");
+
+echo "Rescan Requested";
 
 ?>
+<a href="http://data.mdwestserve.com/syncPackets.php">Process Rescan</a>
+
+
+
+
