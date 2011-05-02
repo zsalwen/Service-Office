1110abc63daee5ce88eb62882ecd4822086bbfb4
zsalwen <zach@mdwestserve.com>
2011-05-02 11:10:52 -0400

    more css

--- ev/webservice.php
+++ ev/webservice.php
@@ -16,29 +16,29 @@ if ($result == false) {
 else {
 		 
 	echo '<table border="1" style="border-collapse:collapse;" width="900px" >';
-	echo '<tr>';
-	echo '<td>Defendant Full Name</td> <td>Address 1</td> <td>Address 2</td> <td>City</td> <td>State</td> <td>Zip</td> <td>Status</td> <td>Status Date</td> ';
+	echo '<tr style="padding:0px; margin:0px;">';
+	echo '<td style="padding:0px; margin:0px;">Defendant Full Name</td> <td style="padding:0px; margin:0px;">Address 1</td> <td style="padding:0px; margin:0px;">Address 2</td> <td style="padding:0px; margin:0px;">City</td> <td style="padding:0px; margin:0px;">State</td> <td style="padding:0px; margin:0px;">Zip</td> <td style="padding:0px; margin:0px;">Status</td> <td style="padding:0px; margin:0px;">Status Date</td> ';
 	echo '</tr>';
 
 	while ($row = @mysql_fetch_array($result,MYSQL_ASSOC)) {
 
 	echo '<tr>';
-	echo '<td valign="top"> 
+	echo '<td valign="top" style="padding:0px; margin:0px;"> 
 	<table style="padding:0px; margin:0px;">
 		<tr>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name1\')">1</td>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name2\')">2</td>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name3\')">3</td>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name4\')">4</td>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name5\')">5</td>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name6\')">6</td>
-	<td>'.$row['defendantfullname'].'</td></tr></table></td> <td valign="top">
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name1\')"><b>1</b></td>
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name2\')"><b>2</b></td>
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name3\')"><b>3</b></td>
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name4\')"><b>4</b></td>
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name5\')"><b>5</b></td>
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="ChgText(\''.$row['defendantfullname'].'\',\'name6\')"><b>6</b></td>
+	<td style="padding:0px; margin:0px;">'.$row['defendantfullname'].'</td></tr></table></td> <td valign="top" style="padding:0px; margin:0px;">
 	<table style="padding:0px; margin:0px;">
 		<tr>
-			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="setAddress1(\''.$row['defendantaddress1'].' '.$row['defendantaddress2'].'\',\''.$row['defendantcity'].'\',\''.$row['defendantstate'].'\',\''.$row['defendantzip'].'\')">1</td>
-	<td>'.$row['defendantaddress1'].'</td></tr></table></td>';
-	echo '<td valign="top">'.$row['defendantaddress2'].'</td> <td valign="top">'.$row['defendantcity'].'</td> <td valign="top">'.$row['defendantstate'].'</td>  <td valign="top">'.$row['defendantzip'].'</td>';
-	echo '<td valign="top">'.$row['status'].'</td> <td valign="top">'.$row['statusdate'].'</td> ';
+			<td class="action" style="cursor:pointer; padding:0px; margin:0px;" onclick="setAddress1(\''.$row['defendantaddress1'].' '.$row['defendantaddress2'].'\',\''.$row['defendantcity'].'\',\''.$row['defendantstate'].'\',\''.$row['defendantzip'].'\')"><b>1</b></td>
+	<td style="padding:0px; margin:0px;">'.$row['defendantaddress1'].'</td></tr></table></td>';
+	echo '<td valign="top" style="padding:0px; margin:0px;">'.$row['defendantaddress2'].'</td> <td valign="top">'.$row['defendantcity'].'</td> <td valign="top">'.$row['defendantstate'].'</td>  <td valign="top">'.$row['defendantzip'].'</td>';
+	echo '<td valign="top" style="padding:0px; margin:0px;">'.$row['status'].'</td> <td valign="top">'.$row['statusdate'].'</td> ';
 		
 		
 	echo '</tr>';
