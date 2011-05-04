37c27d0f69f76a3f45e27475736df37ae4798e2a
Patrick McGuire <insidenothing@gmail.com>
2011-05-03 19:35:34 -0700

    test graph

--- overallGraph.php
+++ overallGraph.php
@@ -58,3 +58,91 @@ mysql_close();
 ?>
 </table>
 <center><b>high,current,low</b></center>
+<hr>
+
+  <head>
+   <title> Service Volume </title>
+   <script language="javascript" src="http://www.google.com/jsapi"></script>
+   </head>
+   <body>
+
+   <div id="chart"></div>
+
+   <script type="text/javascript">
+      var queryString = '';
+      var dataUrl = '';
+
+      function onLoadCallback() {
+        if (dataUrl.length > 0) {
+          var query = new google.visualization.Query(dataUrl);
+          query.setQuery(queryString);
+          query.send(handleQueryResponse);
+        } else {
+          var dataTable = new google.visualization.DataTable();
+          dataTable.addRows(7);
+
+          dataTable.addColumn('number');
+          dataTable.addColumn('number');
+          dataTable.addColumn('number');
+          dataTable.setValue(0, 0, 68.24072192621668);
+          dataTable.setValue(0, 1, 34.56596279744238);
+          dataTable.setValue(0, 2, 30.098);
+          dataTable.setValue(1, 0, 88.56959906724578);
+          dataTable.setValue(1, 1, 70.82607732878787);
+          dataTable.setValue(1, 2, 23.223);
+          dataTable.setValue(2, 0, 91.36680313467521);
+          dataTable.setValue(2, 1, 65.46853881413088);
+          dataTable.setValue(2, 2, 43.467);
+          dataTable.setValue(3, 0, 95.05014894731983);
+          dataTable.setValue(3, 1, 86.29010404393469);
+          dataTable.setValue(3, 2, 53.548);
+          dataTable.setValue(4, 0, 48.556824963465665);
+          dataTable.setValue(4, 1, 73.13114860769234);
+          dataTable.setValue(4, 2, 60.24);
+          dataTable.setValue(5, 0, 49.45816933251623);
+          dataTable.setValue(5, 1, 52.25935572937881);
+          dataTable.setValue(5, 2, 48.786);
+          dataTable.setValue(6, 0, 41.50393106825148);
+          dataTable.setValue(6, 1, 58.291845810651964);
+          dataTable.setValue(6, 2, 10);
+
+          draw(dataTable);
+        }
+      }
+
+      function draw(dataTable) {
+        var vis = new google.visualization.ImageChart(document.getElementById('chart'));
+        var options = {
+          chf: 'bg,s,C2BDDD',
+          chxl: '',
+          chxp: '',
+          chxr: '0,0,90',
+          chxs: '0,676767,10.5,0,l,676767',
+          chxtc: '',
+          chxt: 'y',
+          chbh: 'a',
+          chs: '300x225',
+          cht: 'bvs',
+          chco: 'FF0000,00FF00,3072F3',
+          chd: 'e:rq4q6d80fEfpaj,WHtUp43NuyhclT,TQO2bziQmifNGZ',
+          chdl: 'High+Value|Current+Value|Low+Value',
+          chdlp: 't',
+          chtt: 'Service+Volume'
+        };
+        vis.draw(dataTable, options);
+      }
+
+      function handleQueryResponse(response) {
+        if (response.isError()) {
+          alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+          return;
+        }
+        draw(response.getDataTable());
+      }
+
+      google.load("visualization", "1", {packages:["imagechart"]});
+      google.setOnLoadCallback(onLoadCallback);
+
+    </script>
+  </body>
+
