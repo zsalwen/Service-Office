e8a3f0d903f1eb52e03be74185eb53d3bbee9622
Patrick McGuire <insidenothing@gmail.com>
2011-05-03 19:46:28 -0700

    ok

--- overallGraph.php
+++ overallGraph.php
@@ -151,7 +151,7 @@ mysql_close();
 <table border="1" width="100%">
 <tr>
 <td><?=makeChart('Received to Dispatch','chart1');?></td>
-<td><? //makeChart('Dispatch to Close','chart2'); ?></td>
+<td></td>
 </tr>
 </table>
 
