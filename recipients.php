0503639d3b92fcd757dc678eb2b76c26ed53f1d2
zsalwen <zach@mdwestserve.com>
2011-05-04 16:28:19 -0400

    only displaying links to appropriate envelopes

--- recipients.php
+++ recipients.php
@@ -40,8 +40,19 @@ if ($_POST[edit]){
 	$q="SELECT * FROM envelopeImage ORDER BY envID ASC";
 }
 $r=@mysql_query($q) or die("Query: $q<br>".mysql_error());
-while($d=mysql_fetch_array($r,MYSQL_ASSOC)){ ?>
-	<form method='post'><input type='hidden' name='edit' value='<?=$d[envID]?>'><tr><td><?=$d[envID]?></td><td><?=stripslashes($d[to1])?></td><td><?=stripslashes($d[to2])?></td><td><?=stripslashes($d[to3])?></td><td><?=stripslashes($d[addressType])?><? if ($d[addressType] == 'CLIENT'){ echo "-".getClient($d[envID]); }?></td><td><input style='background-color:pink; height:25px;' type='submit' name='edit2' value='[edit]'></td><td><a href='http://staff.mdwestserve.com/otd/stuffPacket.bgw.php?id=<?=$d[envID]?>' target='_blank'>WHITE</a> <a href='http://staff.mdwestserve.com/otd/stuffPacket.2.php?id=<?=$d[envID]?>&sb=1' target='_blank'>GREEN (S&B)</a></td></tr></form>
+while($d=mysql_fetch_array($r,MYSQL_ASSOC)){ 
+if (strpos(strtoupper($d[to1]),'SHAPIRO & BURSON') !== false){
+	//only display green envelope links for Burson
+	$link="<a href='http://staff.mdwestserve.com/otd/stuffPacket.2.php?id=$d[envID]&sb=1' target='_blank'>GREEN</a>";
+}elseif($d[addressType] == 'CLIENT' || (strpos(strtoupper($d[to1]),'FOR ANNE ARUNDEL COUNTY') !== false) || (strpos(strtoupper($d[to1]),'FOR BALTIMORE CITY') !== false) || (strpos(strtoupper($d[to1]),'FOR DORCHESTER COUNTY') !== false)){
+	//only display white envelope links for other clients, dorchester, baltimore city, and anne arundel
+	$link="<a href='http://staff.mdwestserve.com/otd/stuffPacket.bgw.php?id=$d[envID]' target='_blank'>WHITE</a>";
+}else{
+	//display both links for all else
+	$link="<a href='http://staff.mdwestserve.com/otd/stuffPacket.2.php?id=$d[envID]&sb=1' target='_blank'>GREEN</a>|<a href='http://staff.mdwestserve.com/otd/stuffPacket.bgw.php?id=$d[envID]' target='_blank'>WHITE</a>";
+}
+?>
+	<form method='post'><input type='hidden' name='edit' value='<?=$d[envID]?>'><tr><td><?=$d[envID]?></td><td><?=stripslashes($d[to1])?></td><td><?=stripslashes($d[to2])?></td><td><?=stripslashes($d[to3])?></td><td><?=stripslashes($d[addressType])?><? if ($d[addressType] == 'CLIENT'){ echo "-".getClient($d[envID]); }?></td><td><input style='background-color:pink; height:25px;' type='submit' name='edit2' value='[edit]'></td><td><?=$link?></td></tr></form>
 <?}
 echo "</table>";
 error_log("[".date('h:iA n/j/y')."] ".$_COOKIE[psdata][name]." Viewing Envelope Recipients \n",3,"/logs/user.log");
