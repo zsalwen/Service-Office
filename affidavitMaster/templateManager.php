df74f839c640f708b3132ae4726e8584924561c8
zsalwen <zach@mdwestserve.com>
2011-05-13 12:33:43 -0400

    sudo rsync, ssh to mdws2 before backup

--- affidavitMaster/templateManager.php
+++ affidavitMaster/templateManager.php
@@ -67,12 +67,12 @@ if($_GET[affidavit] || $_POST[affidavit]){
 		$fh = fopen($fullPath, 'w') or die("can't open file: [$fullPath]");
 		fwrite($fh, $whiteboard);
 		fclose($fh);
-		$cmd='rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates';
+		$cmd='sudo rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates';
 		$last_line = system($cmd,$retval);
 		if ($retval || $last_line){
 			echo "<h1>CMD: $cmd<br>RET: $retval<br>LAST: $last_line</h1>";
 		}
-		$cmd2='ssh 10.0.0.2 chown root:root /data/service/templates; ssh 10.0.0.2 chmod 777 /data/service/templates';
+		$cmd2='ssh mdws2.mdwestserve.com; ssh 10.0.0.2 chown root:root /data/service/templates; ssh 10.0.0.2 chmod 777 /data/service/templates; exit;';
 		$last_line2 = system($cmd2,$retval2);
 		if ($retval2 || $last_line2){
 			echo "<h1>CMD: $cmd2<br>RET: $retval2<br>LAST: $last_line2</h1>";
