cd2c749f170fa18891cda901d40a2dd1d09a1372
zsalwen <zach@mdwestserve.com>
2011-05-13 13:24:57 -0400

    splitting rsync and ssh into two different commands

--- affidavitMaster/templateManager.php
+++ affidavitMaster/templateManager.php
@@ -67,12 +67,16 @@ if($_GET[affidavit] || $_POST[affidavit]){
 		$fh = fopen($fullPath, 'w') or die("can't open file: [$fullPath]");
 		fwrite($fh, $whiteboard);
 		fclose($fh);
-		$cmd="sudo rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates;
-		ssh mdws2.mdwestserve.com ssh 10.0.0.2 chown root:root /data/service/templates &;
+		$cmd="sudo rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates;"
+		$cmd2="ssh mdws2.mdwestserve.com ssh 10.0.0.2 chown root:root /data/service/templates &;
 		ssh mdws2.mdwestserve.com ssh 10.0.0.2 chmod 777 /data/service/templates &;";
 		$last_line = system($cmd,$retval);
 		if ($retval || $last_line){
-			echo "<h1>CMD: $cmd<br>RET: $retval<br>LAST: $last_line</h1>";
+			echo "<h3>CMD: $cmd<br>RET: $retval<br>LAST: $last_line</h3>";
+		}
+		$last_line2 = system($cmd2,$retval2);
+		if ($retval2 || $last_line2){
+			echo "<h3>CMD: $cmd2<br>RET: $retval2<br>LAST: $last_line2</h3>";
 		}
 		$saved=1;
 	}
