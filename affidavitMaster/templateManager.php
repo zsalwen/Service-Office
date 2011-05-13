46bb6261b7be8f29b71ca101117af1843d355770
zsalwen <zach@mdwestserve.com>
2011-05-13 14:28:00 -0400

    trying sudo again

--- affidavitMaster/templateManager.php
+++ affidavitMaster/templateManager.php
@@ -67,7 +67,7 @@ if($_GET[affidavit] || $_POST[affidavit]){
 		$fh = fopen($fullPath, 'w') or die("can't open file: [$fullPath]");
 		fwrite($fh, $whiteboard);
 		fclose($fh);
-		$cmd="rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates;";
+		$cmd="sudo rsync -z /data/service/templates/* mdws2.mdwestserve.com:/data/service/templates;";
 		$cmd2="ssh root@mdws2.mdwestserve.com ssh root@10.0.0.2 chown -R root:root /data/service/templates&;
 		ssh root@mdws2.mdwestserve.com ssh root@10.0.0.2 chmod -R 777 /data/service/templates&;";
 		$last_line = system($cmd,$retval);
