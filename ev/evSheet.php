45405b02ae01908fbe979f0f6b06efafbd3992b3
zsalwen <zach@mdwestserve.com>
2011-05-16 13:47:38 -0400

    style

--- ev/evSheet.php
+++ ev/evSheet.php
@@ -377,8 +377,8 @@ function evSheet($eviction){
 			<td><?=$d[case_no]?></td>
 		</tr>
 		<? if ($d[attorneys_id] == 1){?>
-		<tr style='border-top: 1px solid black;'>
-			<td align='center' colspan=5' style='font-weight:bold;'><small><?=strtoupper($d[name1])?> - <?=strtoupper($d[address1])?>, <?=strtoupper($d[city1])?>, <?=strtoupper($d[state1])?> <?=strtoupper($d[zip1])?></small></td>
+		<tr>
+			<td align='center' colspan=5' style='font-weight:bold; border-top:1px solid black;'><small><?=strtoupper($d[name1])?> - <?=strtoupper($d[address1])?>, <?=strtoupper($d[city1])?>, <?=strtoupper($d[state1])?> <?=strtoupper($d[zip1])?></small></td>
 		</tr>
 		<? } ?>
 	</table>
