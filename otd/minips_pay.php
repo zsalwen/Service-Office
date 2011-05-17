fda304aa28cf8c0040ab4e2af267043e5cb7dbf3
zsalwen <zach@mdwestserve.com>
2011-05-17 12:34:53 -0400

    removing process service account details table width

--- otd/minips_pay.php
+++ otd/minips_pay.php
@@ -209,7 +209,7 @@ echo "<center><div>[GAS: $d1[gasPrice] | CLIENT: $d1[client_rate]| CONTRACTOR: $
 <table><tr><td width='50%'>
 <fieldset>
 	<legend>Process Service Account Details</legend>
-<table width="100%">
+<table>
 	<tr>
     	<td></td>
         <td style="font-size:12px;"><?=id2name($data[server_id])?></td>
