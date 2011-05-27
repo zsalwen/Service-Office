61a3408d5859c449ec5b869c6eb7737ada1a6803
zsalwen <zach@mdwestserve.com>
2011-05-27 13:31:07 -0700

    Implementing Steve's newest language improvements

--- textGen.php
+++ textGen.php
@@ -92,13 +92,13 @@ if (isset($_POST['server'])){
 	$i=0;
 	if ($d['server_id'] == $serverSlot){
 		$addList = strtoupper($d[address1])."<br>".strtoupper($d[city1]).", ".strtoupper($d[state1])." ".$d[zip1]."<br>";
-		$addList2[$i] = "1 address in ".strtoupper($d[city1]).", ".strtoupper($d[state1]);
+		$addList2[$i] = "in ".strtoupper($d[city1]).", ".strtoupper($d[state1]);
 		$addCount++;
 		$i++;
 	}
 	if ($d['server_ida'] == $serverSlot){
 		$addList = strtoupper($d[address1a])."<br>".strtoupper($d[city1a]).", ".strtoupper($d[state1a])." ".$d[zip1a]."<br>";
-		$addList2[$i] = "1 address in ".strtoupper($d[city1a]).", ".strtoupper($d[state1a]);
+		$addList2[$i] = "in ".strtoupper($d[city1a]).", ".strtoupper($d[state1a]);
 		$addCount++;
 		$i++;
 	}
@@ -107,7 +107,7 @@ if (isset($_POST['server'])){
 			$addList .= "     --AND--<br>";
 		}
 		$addList .= strtoupper($d[address1b])."<br>".strtoupper($d[city1b]).", ".strtoupper($d[state1b])." ".$d[zip1b]."<br>";
-		$addList2[$i] .= "1 address in ".strtoupper($d[city1b]).", ".strtoupper($d[state1b]);
+		$addList2[$i] .= "in ".strtoupper($d[city1b]).", ".strtoupper($d[state1b]);
 		$addCount++;
 		$i++;
 	}
@@ -116,7 +116,7 @@ if (isset($_POST['server'])){
 			$addList .= "     --AND--<br>";
 		}
 		$addList .= strtoupper($d[address1c])."<br>".strtoupper($d[city1c]).", ".strtoupper($d[state1c])." ".$d[zip1c]."<br>";
-		$addList2[$i] .= "1 address in ".strtoupper($d[city1c]).", ".strtoupper($d[state1c]);
+		$addList2[$i] .= "in ".strtoupper($d[city1c]).", ".strtoupper($d[state1c]);
 		$addCount++;
 		$i++;
 	}
@@ -125,7 +125,7 @@ if (isset($_POST['server'])){
 			$addList .= "     --AND--<br>";
 		}
 		$addList .= strtoupper($d[address1d])."<br>".strtoupper($d[city1d]).", ".strtoupper($d[state1d])." ".$d[zip1d]."<br>";
-		$addList2[$i] .= "1 address in ".strtoupper($d[city1d]).", ".strtoupper($d[state1d]);
+		$addList2[$i] .= "in ".strtoupper($d[city1d]).", ".strtoupper($d[state1d]);
 		$addCount++;
 		$i++;
 	}
@@ -134,11 +134,11 @@ if (isset($_POST['server'])){
 			$addList .= "     --AND--<br>";
 		}
 		$addList .= strtoupper($d[address1e])."<br>".strtoupper($d[city1e]).", ".strtoupper($d[state1e])." ".$d[zip1e]."<br>";
-		$addList2[$i] .= "1 address in ".strtoupper($d[city1e]).", ".strtoupper($d[state1e]);
+		$addList2[$i] .= "in ".strtoupper($d[city1e]).", ".strtoupper($d[state1e]);
 		$addCount++;
 	}
 	
-	$addList3='a file with ';
+	//$addList3='a file with ';
 	$t=0;
 	$andLength=count($addList2)-1;
 	while ($t < count($addList2)){
@@ -166,12 +166,6 @@ $r3=@mysql_query($q3) or die ("Query: $q3<br>".mysql_error());
 $d3=mysql_fetch_array($r3, MYSQL_ASSOC);
 $now = date ('H');
 
-if ($now < 12)
-$dayTime='morning';
-else if ($now < 18)
-$dayTime='afternoon';
-else if ($now >= 18 )
-$dayTime='evening';
 $otdStr=str_replace('portal//var/www/dataFiles/service/orders/','PS_PACKETS/',$d[otd]);
 $otdStr=str_replace('data/service/orders/','PS_PACKETS/',$otdStr);
 $otdStr=str_replace('portal/','',$otdStr);
@@ -209,12 +203,12 @@ $instructionLink .= ".php?packet=".$d[packet_id];
 	</tr>
 	<tr>
 		<td>I spoke on the phone with <? if (isset($_POST['server'])){ echo $d2[name];}else{echo "<i><u>SERVER NOT YET SELECTED</u></i>";} ?>
-		this <?=$dayTime?>, concerning process service on <? if (isset($_POST['server'])){echo $addList3;}else{echo "<i>ADDRESS NOT YET SELECTED</i>";}?>.<br>
-		I was quoted a price of <? if (isset($_POST['server'])){echo "<i><b>INSERT SERVICE COST</b></i>"; }else{echo "<i><u>SERVER NOT YET SELECTED</u></i>";} ?>
+		today concerning process service <? if (isset($_POST['server'])){echo $addList3;}else{echo "<i>ADDRESS NOT YET SELECTED</i>";}?>.<br>
+		I was quoted a price of <? if (isset($_POST['server'])){echo "<i><b>[$$$$]</b></i>"; }else{echo "<i><u>SERVER NOT YET SELECTED</u></i>";} ?>
 		for two attempts on <? if ($defCount == '1'){ echo "1 defendant";}else{ echo $defCount." defendants";}?> at 
 		<? if (isset($addCount) && $addCount == '1'){echo "1 address";}elseif(isset($addCount) && $addCount > 1){echo $addCount." addresses";
-		}else{ echo "<i>NO SERVER SELECTED</i>";}?>, which would make the total cost of service <i><b>INSERT SERVICE TOTAL</b></i>.<br>
-		I have attached the documents for service to this email, along with an instruction sheet for easy reference (you can disregard the names of any other servers/addresses, as they only concern MD service).  After you have completed service, please call our office so we can prepare our affidavits for you.<br><br>
+		}else{ echo "<i>NO SERVER SELECTED</i>";}?>, and <i><b>[$$$$]</b></i> for overnight delivery of affidavits back to our office, which would make the total cost of service <i><b>INSERT SERVICE TOTAL</b></i>.<br>
+		I have included the documents for service in an USPS Express Mailer, along with an instruction sheet for easy reference (you can disregard the names of any other servers/addresses, as they only concern MD service).  The tracking number of the package is <b style='background-color:#FFFF00;'>[####]</b>.  <b>You should be receiving the documents sometime tomorrow; can you please email or call our office when the documents are received?</b><br><br>
 		<? 
 		if ($d[lossMit] != '' && $d[lossMit] != 'N/A - OLD L'){
 			if ($d[attorneys_id] == 1){
@@ -223,42 +217,42 @@ $instructionLink .= ".php?packet=".$d[packet_id];
 				$toCounty=county2envelope2($d[circuit_court]);
 				if ($d[lossMit] != 'FINAL'){
 					//if preliminary, instruct to include one envelope to client
-					$lossMit="Per Maryland law HB472, please include one of the provided GREEN, preprinted #10 envelopes addressed to '$toAttorney', with the service documents for each defendant.";
+					$lossMit="Per Maryland law HB472, <b>please include one green #10 envelope addressed to '$toAttorney'  with each defendant's service documents</b>.";
 				}else{
 					//if final, instruct to include two envelopes: one to court and one to client
-					$lossMit = "Per Maryland law HB472, two envelopes should be included with the service documents for each defendant (one GREEN envelope addressed to '$toAttorney', and one GREEN envelope addressed to '$toCounty').";
+					$lossMit = "Per Maryland law HB472, <b>please include one green #10 envelope addressed to '$toAttorney', AND one green #10 envelope addressed to '$toCounty' with each defendant's service documents</b>.";
 				}
 			}else{
 				//if file requires white BGW-style envelopes, then instruct to include envelope for attorney
 				$toAttorney=id2attorneyName($d[attorneys_id]);
-				$lossMit="Per Maryland law HB472, please include one of the provided white, preprinted #10 envelope addressed to '$toAttorney'";
+				$lossMit="Per Maryland law HB472, <b>please include one white #10 envelope addressed to '$toAttorney'";
 				if ($d[lossMit] == 'FINAL'){
 					//if file is a final, instruct to include envelope for court
 					$toCounty=county2envelope2($d[circuit_court]);
-					$lossMit .= " and another white, preprinted #10 envelope addressed to '$toCounty'";
+					$lossMit .= " AND one white #10 envelope addressed to '$toCounty'";
 				}
-				$lossMit .= " with each defendant's service documents.";
+				$lossMit .= "</b> with each defendant's service documents.";
 			}
-			echo $lossMit."<br>";
+			echo $lossMit."  Also, per new Maryland state requirements <b style='background-color:#FFFF00;'>please separate service attempts by at least 24 hours and try and make one attempt before noon and one after 6pm</b><br>";
 		}
 		?>
 		The documents for service can be listed on the affidavit as "a copy of the <?if ($d[addlDocs] != ''){echo $d[addlDocs];}else{ echo "Order to Docket";} ?> and all other papers filed with it in the above-captioned case".<br>
 		<b>Packet <?=$d['packet_id']?> - Service on <?=$defNames?></b><br>
 <? if (isset($_POST['server'])){echo $addList; }else{echo "<i>ADDRESS NOT YET SELECTED</i>";}?><br>
-<div style='background-color: pink; font-weight:bold;'>PLEASE SERVE ONLY LISTED ADDRESSES.  ADDITIONALLY, IF THE ADDRESS IS A BUSINESS, APPEARS VACANT, THE DEFENDANTS ARE NOT KNOWN AT THE ADDRESS, OR ADDITIONAL ADDRESSES ARE DISCOVERED BEYOND THOSE LISTED, PLEASE NOTIFY OUR OFFICE <U>BEFORE TAKING ANY FURTHER ACTION</U> FOR DIRECTIONS ON HOW TO PROCEED.</div><br>
+<div style='background-color: #FF0000; font-weight:bold;'>PLEASE SERVE ONLY LISTED ADDRESSES.  ADDITIONALLY, IF THE ADDRESS IS A BUSINESS, APPEARS VACANT, THE DEFENDANTS ARE NOT KNOWN AT THE ADDRESS, OR ADDITIONAL ADDRESSES ARE DISCOVERED BEYOND THOSE LISTED, PLEASE NOTIFY OUR OFFICE BEFORE TAKING ANY FURTHER ACTION FOR DIRECTIONS ON HOW TO PROCEED.</div><br>
 As each step of service is performed, please call or email our office with updates.  Also, upon completion of service, please contact us with the details of how service was completed.  Service can be completed by:<br><br>		
 Personal delivery (only can be achieved in the following manner):<br>
 1.  Delivery to the defendants themselves.<br>
-2.  OR delivery to someone <b>over the age of 18</b> who resides with the defendant in question at the address being served.<br>
+2.  OR delivery to someone over the age of 18 who resides with the defendant in question at the address being served.<br>
 <b>For ALL personal delivery service Maryland law REQUIRES a physical description of the individual served.</b><br><br>
 Non-Service (if personal delivery cannot be effected):<br>
-1.  Take note of the date and time of the attempts made, as well as a description of the residence at which service is being attempted.  <b>For ALL non-service affidavits Maryland law REQUIRES a physical description of the residence.</b><br><br>
-REGARDLESS OF SERVICE ACHIEVED, after it is complete, CALL our office with the details and we will prepare affidavits, and send them for you to sign, notarize, and <b>RETURN VIA OVERNIGHT SHIPPING TO OUR OFFICE:</b><br><br>
+1.  Take note of the date and time of the attempts made, as well as a description of the residence at which service is being attempted.<br>
+<b>For ALL non-service affidavits Maryland law REQUIRES a physical description of the residence.</b><br><br>
+<div style='background-color:#FFFF00;'>REGARDLESS OF SERVICE ACHIEVED, after it is complete, CALL OR EMAIL our office with the details and WE WILL PREPARE THE AFFIDAVITS, and send them for you to sign, notarize, and RETURN VIA OVERNIGHT SHIPPING TO OUR OFFICE:</div><br>
 <b>MDWestServe, Inc.<br>
 300 E. Joppa Road Suite 1102<br>
 Towson, MD 21286</b><br><br>
-	If there are any difficulties with this service, please have the server directly contact our office from the field.  During business hours (9-5 EST), please contact Alex at 410-828-4569.  Patrick, our Operations Manager, can be reached after hours at (443) 386-2584, please do not hesitate to call him.<br>
-Lastly, around 6 PM EST you should receive a confirmation email of all files sent from our office today.  If you have any issues with the documents or instructions that we have sent you, you must contact our office within 24 hours of receipt, or your office will be held responsible for any delays.<br><br>
+	If there are any difficulties with this service, please have the server directly contact our office from the field.  During business hours (9-5 EST), please contact Alex at 410-828-4569.  Patrick, our Operations Manager, can be reached after hours at (443) 386-2584, please do not hesitate to call him.  Lastly, around 6 PM EST you should receive a confirmation email of all files sent from our office today.  If you have any issues with the documents or instructions that we have sent you, you must contact our office within 24 hours of receipt, or your office will be held responsible for any delays.<br><br>
 Please let either myself or Patrick know if there are any questions or issues.<br><br>
 
 Thank you,<br>
