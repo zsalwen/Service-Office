66afb99806c3362e018bd0a9485d54da8935e23c
Patrick McGuire <insidenothing@gmail.com>
2011-06-01 18:59:43 -0700

    Edited dailyITtodo.php via GitHub

--- dailyITtodo.php
+++ dailyITtodo.php
@@ -1,35 +1,28 @@
 <?
-
-
-$loginURL = "http://github.com/api/v2/json/issues/list/MDWestServe-Inc/Service-Office/open";
-
+function processsOpen($repo){
+$loginURL = "http://github.com/api/v2/json/issues/list/MDWestServe-Inc/$repo/open";
 $curl = curl_init();
-// Set options
 curl_setopt ($curl, CURLOPT_URL, $loginURL);
 curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
-
 curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
-
-
-
 curl_setopt( $curl, CURLOPT_COOKIEJAR, $cookie );
-
     curl_setopt( $curl, CURLOPT_ENCODING, "" );
     curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
     curl_setopt( $curl, CURLOPT_AUTOREFERER, true );
     curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
-
-
-
 $buffer = curl_exec ($curl);
-
-
-
-
 //if all goes well
-
-
-echo "<pre>";
+echo "<h1>$repo</h1><pre>";
 var_dump(json_decode($buffer, true));
 echo "</pre>";
+}
+
+processOpen('Service-Office');
+processOpen('Service-Interface');
+processOpen('Service-Web-Service');
+processOpen('Service-Client-Access');
+processOpen('Mobile-Service');
+processOpen('Service-Accounting');
+processOpen('Verify');
+
 ?>
